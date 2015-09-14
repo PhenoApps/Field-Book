d7d93cf411d952bd0e5da8c12f8b7b08d2ee4754
trife <trife@users.noreply.github.com>
2015-09-14 16:53:57 -0500

    Miscellaneous code fixes and cleanup

--- Field Book/app/src/main/java/com/fieldbook/tracker/Constants.java
+++ Field Book/app/src/main/java/com/fieldbook/tracker/Constants.java
@@ -6,30 +6,30 @@ import java.io.File;
 
 public class Constants {
     public static final String RESOURCEPATH = Environment.getExternalStorageDirectory()
-            + "/FieldBook/resources";
+            + "/fieldBook/resources";
 
     public static String PLOTDATAPATH = Environment.getExternalStorageDirectory()
-            + "/FieldBook/plot_data";
+            + "/fieldBook/plot_data";
 
     public static String TRAITPATH = Environment.getExternalStorageDirectory()
-            + "/FieldBook/trait";
+            + "/fieldBook/trait";
 
     public static String FIELDIMPORTPATH = Environment.getExternalStorageDirectory()
-            + "/FieldBook/field_import";
+            + "/fieldBook/field_import";
 
     public static String FIELDEXPORTPATH = Environment.getExternalStorageDirectory()
-            + "/FieldBook/field_export";
+            + "/fieldBook/field_export";
 
     public static final File MPATH = new File(Environment.getExternalStorageDirectory()
-            + "/FieldBook");
+            + "/fieldBook");
 
     public static String BACKUPPATH = Environment.getExternalStorageDirectory()
-            + "/FieldBook/database";
+            + "/fieldBook/database";
 
     public static String ERRORPATH = Environment.getExternalStorageDirectory()
-            + "/FieldBook/errors";
+            + "/fieldBook/errors";
 
     public static String UPDATEPATH = Environment.getExternalStorageDirectory()
-            + "/FieldBook/updates";
+            + "/fieldBook/updates";
 
 }
