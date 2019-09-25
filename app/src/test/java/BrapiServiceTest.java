3e0be102b396cb0d007843deeee12764cc186fa4
Chris T <ct447@cornell.edu>
2019-09-25 08:39:51 -0400

    Added tests for each brapi endpoint

--- app/src/test/java/BrapiServiceTest.java
+++ app/src/test/java/BrapiServiceTest.java
@@ -1,32 +1,24 @@
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 
 import androidx.arch.core.util.Function;
-import androidx.test.core.app.ApplicationProvider;
-import androidx.test.platform.app.InstrumentationRegistry;
 
-import com.fieldbook.tracker.ConfigActivity;
-import com.fieldbook.tracker.R;
 import com.fieldbook.tracker.brapi.BrAPIService;
-import com.fieldbook.tracker.brapi.BrapiActivity;
+import com.fieldbook.tracker.brapi.BrapiListResponse;
+import com.fieldbook.tracker.brapi.BrapiStudyDetails;
 import com.fieldbook.tracker.brapi.BrapiStudySummary;
-import com.fieldbook.tracker.preferences.PreferencesActivity;
-import com.fieldbook.tracker.utilities.Constants;
+import com.fieldbook.tracker.traits.TraitObject;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.RuntimeEnvironment;
-import org.robolectric.annotation.Config;
+
 
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.locks.Condition;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -34,24 +26,29 @@ import static org.junit.Assert.fail;
 public class BrapiServiceTest {
 
     String brapiBaseUrl = "https://test-server.brapi.org/brapi/v1";
+    BrAPIService brAPIService;
     Boolean checkGetStudiesResult = false;
+    Boolean checkGetStudyDetailsResult = false;
+    Boolean checkGetPlotDetailsResult = false;
+    Boolean checkGetTraitsResult = false;
+    Boolean checkGetOntologyResult = false;
+
 
     @Before
     public void setUp() throws Exception {
-
+        // Instantiate our brapi service class
+        this.brAPIService = new BrAPIService(brapiBaseUrl, null);
     }
 
     @Test
     public void checkGetStudies() {
 
-        // Instantiate our brapi service class
-        BrAPIService brapiService = new BrAPIService(brapiBaseUrl, null);
 
         // Set up our signal to wait for the callback to be called.
         final CountDownLatch signal = new CountDownLatch(1);
 
         // Call our get studies endpoint with the same parsing that our classes use.
-        brapiService.getStudies(new Function<List<BrapiStudySummary>, Void>() {
+        this.brAPIService.getStudies(new Function<List<BrapiStudySummary>, Void>() {
             @Override
             public Void apply(List<BrapiStudySummary> input) {
                 // Check that there is atleast one study returned.
@@ -86,42 +83,162 @@ public class BrapiServiceTest {
     @Test
     public void checkGetStudyDetails(){
 
-        // Instantiate our brapi service class
+        // Set up our signal to wait for the callback to be called.
+        final CountDownLatch signal = new CountDownLatch(1);
+        final String studyDbId = "1001";
 
         // Call our get study details endpoint with the same parsing that our classes use.
+        this.brAPIService.getStudyDetails(studyDbId, new Function<BrapiStudyDetails, Void>() {
+            @Override
+            public Void apply(BrapiStudyDetails input) {
+                // Check that the study db id we passed is what we are getting back
+                BrapiServiceTest.this.checkGetStudyDetailsResult = input.getStudyDbId().equals(studyDbId);
 
-        // What are we checking for?
+                // Notify the countdown that we are finish
+                signal.countDown();
+
+                return null;
+            }
+        }, new Function<String, Void>() {
+            @Override
+            public Void apply(String input) {
+                BrapiServiceTest.this.checkGetStudyDetailsResult = false;
+                // Notify the countdown that we are finish
+                signal.countDown();
+                return null;
+            }
+        });
+
+        // Wait for our callback and evaluate how we did
+        try {
+            signal.await();
+            assertTrue(BrapiServiceTest.this.checkGetStudyDetailsResult);
+        }
+        catch (InterruptedException e) {
+            fail(e.toString());
+        }
 
     }
 
     @Test
     public void checkGetPlotDetails(){
 
-        // Instantiate our brapi service class
-
         // Call our get plot details endpoint with the same parsing that our classes use.
+        // Set up our signal to wait for the callback to be called.
+        final CountDownLatch signal = new CountDownLatch(1);
+        final String studyDbId = "1001";
 
-        // What are we checking for?
+        // Call our get study details endpoint with the same parsing that our classes use.
+        this.brAPIService.getPlotDetails(studyDbId, new Function<BrapiStudyDetails, Void>() {
+            @Override
+            public Void apply(BrapiStudyDetails input) {
+                // Check that we are getting some results back
+                BrapiServiceTest.this.checkGetPlotDetailsResult = input.getNumberOfPlots() > 0;
+
+                // Notify the countdown that we are finish
+                signal.countDown();
+
+                return null;
+            }
+        }, new Function<String, Void>() {
+            @Override
+            public Void apply(String input) {
+                BrapiServiceTest.this.checkGetPlotDetailsResult = false;
+                // Notify the countdown that we are finish
+                signal.countDown();
+                return null;
+            }
+        });
+
+        // Wait for our callback and evaluate how we did
+        try {
+            signal.await();
+            assertTrue(BrapiServiceTest.this.checkGetPlotDetailsResult);
+        }
+        catch (InterruptedException e) {
+            fail(e.toString());
+        }
 
     }
 
     @Test
     public void checkGetOntology(){
 
-        // Instantiate our brapi service class
+        // Call our get plot details endpoint with the same parsing that our classes use.
+        // Set up our signal to wait for the callback to be called.
+        final CountDownLatch signal = new CountDownLatch(1);
 
-        // Call our get ontology endpoint with the same parsing that our classes use.
+        // Call our get study details endpoint with the same parsing that our classes use.
+        this.brAPIService.getOntology(null, null, new Function<BrapiListResponse<TraitObject>, Void>() {
+            @Override
+            public Void apply(BrapiListResponse<TraitObject> input) {
+                // Check that we are getting some results back
+                BrapiServiceTest.this.checkGetOntologyResult = input.getData().size() > 0;
 
-        // What are we checking for?
+                // Notify the countdown that we are finish
+                signal.countDown();
+
+                return null;
+            }
+        }, new Function<String, Void>() {
+            @Override
+            public Void apply(String input) {
+                BrapiServiceTest.this.checkGetOntologyResult = false;
+                // Notify the countdown that we are finish
+                signal.countDown();
+                return null;
+            }
+        });
+
+        // Wait for our callback and evaluate how we did
+        try {
+            signal.await();
+            assertTrue(BrapiServiceTest.this.checkGetOntologyResult);
+        }
+        catch (InterruptedException e) {
+            fail(e.toString());
+        }
+
     }
 
     @Test
     public void checkGetTraits(){
 
-        // Instantiate our brapi service class
+        // Call our get plot details endpoint with the same parsing that our classes use.
+        // Set up our signal to wait for the callback to be called.
+        final CountDownLatch signal = new CountDownLatch(1);
+        final String studyDbId = "1001";
 
-        // Call our get traits endpoint with the same parsing that our classes use.
+        // Call our get study details endpoint with the same parsing that our classes use.
+        this.brAPIService.getTraits(studyDbId, new Function<BrapiStudyDetails, Void>() {
+            @Override
+            public Void apply(BrapiStudyDetails input) {
+                // Check that we are getting some results back
+                BrapiServiceTest.this.checkGetTraitsResult = input.getTraits().size() > 0;
 
-        // What are we checking for?
+                // Notify the countdown that we are finish
+                signal.countDown();
+
+                return null;
+            }
+        }, new Function<String, Void>() {
+            @Override
+            public Void apply(String input) {
+                BrapiServiceTest.this.checkGetTraitsResult = false;
+                // Notify the countdown that we are finish
+                signal.countDown();
+                return null;
+            }
+        });
+
+        // Wait for our callback and evaluate how we did
+        try {
+            signal.await();
+            assertTrue(BrapiServiceTest.this.checkGetTraitsResult);
+        }
+        catch (InterruptedException e) {
+            fail(e.toString());
+        }
+
     }
 }
