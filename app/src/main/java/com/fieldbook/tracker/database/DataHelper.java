c00d61a8438bff0a20c6c87f69835762608f88f0
Peter Selby <ps664@cornell.edu>
2020-04-30 21:58:23 -0400

    fix issue #112, reintroduced by commit 0b77e3e66b0b4b94fbc217961ccea3062b1db79d

--- app/src/main/java/com/fieldbook/tracker/database/DataHelper.java
+++ app/src/main/java/com/fieldbook/tracker/database/DataHelper.java
@@ -437,7 +437,7 @@ public class DataHelper {
                 "FROM " +
                 "user_traits " +
                 "JOIN " +
-                "range ON user_traits.rid = range.plot " +
+                "range ON user_traits.rid = range.observationUnitDbId " +
                 "JOIN " +
                 "traits ON user_traits.parent = traits.trait " +
                 "JOIN " +
@@ -506,7 +506,7 @@ public class DataHelper {
                 "FROM " +
                 "user_traits " +
                 "JOIN " +
-                "range ON user_traits.rid = range.plot " +
+                "range ON user_traits.rid = range.observationUnitDbId " +
                 "JOIN " +
                 "traits ON user_traits.parent = traits.trait " +
                 "JOIN " +
