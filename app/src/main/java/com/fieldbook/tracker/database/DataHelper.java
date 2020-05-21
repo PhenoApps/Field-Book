1152f381d8befe51934d474a008945f652c3f656
Trevor Rife <trife@users.noreply.github.com>
2020-05-21 14:32:27 -0500

    Timestamp format fix

--- app/src/main/java/com/fieldbook/tracker/database/DataHelper.java
+++ app/src/main/java/com/fieldbook/tracker/database/DataHelper.java
@@ -87,7 +87,7 @@ public class DataHelper {
             this.insertTraits = db.compileStatement(INSERTTRAITS);
             this.insertUserTraits = db.compileStatement(INSERTUSERTRAITS);
 
-            timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ",
+            timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSXXX",
                     Locale.getDefault());
 
 
