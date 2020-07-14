bde50a2b6a2bb701f3fe297f0051eb04b4d7a7ac
Nahuel Soldevilla <nahuel@leafnode.io>
2020-07-14 15:44:57 -0300

    BrAPI: Filter fields by program and trial
    - New options menu in BrAPI Select fields activity:
     - Filter by program
     - Filter by trial
    - New Select Programs activity: select one programDbId and return it
    - New Select Trial activity:
     - Trial list filtered by programDbId (if available).
     - Select one trialDbId and return it.
    - BrAPIService:
     - getPrograms
     - getTrials
     - getStudies: filter by programDbId and trialDbId
    
    Fix #134
    IBP-3880

--- app/src/main/java/com/fieldbook/tracker/activities/BrapiActivity.java
+++ app/src/main/java/com/fieldbook/tracker/activities/BrapiActivity.java
@@ -1,11 +1,8 @@
 package com.fieldbook.tracker.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.arch.core.util.Function;
-
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,13 +11,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.arch.core.util.Function;
+
+import com.fieldbook.tracker.R;
 import com.fieldbook.tracker.brapi.BrAPIService;
 import com.fieldbook.tracker.brapi.BrapiLoadDialog;
 import com.fieldbook.tracker.brapi.BrapiStudySummary;
 import com.fieldbook.tracker.database.DataHelper;
-import com.fieldbook.tracker.R;
 import com.fieldbook.tracker.utilities.Utils;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +33,13 @@ public class BrapiActivity extends AppCompatActivity {
 
     private BrAPIService brAPIService;
     private BrapiStudySummary selectedStudy;
+
+    // Filter by
+    private String programDbId;
+    private String trialDbId;
+    private static final int FILTER_BY_PROGRAM_REQUEST_CODE = 1;
+    private static final int FILTER_BY_TRIAL_REQUEST_CODE = 2;
+    public static final String PROGRAM_DB_ID_INTENT_PARAM = "programDbId";
 
     @Override
     public void onDestroy() {
@@ -68,10 +76,16 @@ public class BrapiActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_fields_brapi, menu);
+        return true;
+    }
+
     private void loadToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        if(getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(null);
             getSupportActionBar().getThemedContext();
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -84,7 +98,7 @@ public class BrapiActivity extends AppCompatActivity {
         listStudies.setVisibility(View.GONE);
         findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
 
-        brAPIService.getStudies(BrAPIService.getBrapiToken(this), new Function<List<BrapiStudySummary>, Void>() {
+        brAPIService.getStudies(BrAPIService.getBrapiToken(this), this.programDbId, this.trialDbId, new Function<List<BrapiStudySummary>, Void>() {
             @Override
             public Void apply(final List<BrapiStudySummary> studies) {
 
@@ -160,10 +174,39 @@ public class BrapiActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.filter_by_program:
+                Intent filterByProgramIntent = new Intent(this, BrapiProgramActivity.class);
+                startActivityForResult(filterByProgramIntent, FILTER_BY_PROGRAM_REQUEST_CODE);
+                break;
+            case R.id.filter_by_trial:
+                Intent filterByTrialIntent = new Intent(this, BrapiTrialActivity.class);
+                filterByTrialIntent.putExtra(PROGRAM_DB_ID_INTENT_PARAM, programDbId);
+                startActivityForResult(filterByTrialIntent, FILTER_BY_TRIAL_REQUEST_CODE);
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == FILTER_BY_PROGRAM_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                programDbId = data.getDataString();
+                // reset previous filter
+                trialDbId = null;
+                loadStudiesList();
+            }
+        } else if (requestCode == FILTER_BY_TRIAL_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                trialDbId = data.getDataString();
+                loadStudiesList();
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/fieldbook/tracker/activities/BrapiTrialActivity.java
@@ -0,0 +1,142 @@
+package com.fieldbook.tracker.activities;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.arch.core.util.Function;
+
+import com.fieldbook.tracker.R;
+import com.fieldbook.tracker.brapi.BrAPIService;
+import com.fieldbook.tracker.brapi.BrapiTrial;
+import com.fieldbook.tracker.database.DataHelper;
+import com.fieldbook.tracker.utilities.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrapiTrialActivity extends AppCompatActivity {
+    private BrAPIService brAPIService;
+    private BrapiTrial brapiTrial;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Utils.isConnected(this)) {
+            if (BrAPIService.hasValidBaseUrl(this)) {
+                setContentView(R.layout.activity_brapi_trials);
+                String brapiBaseURL = BrAPIService.getBrapiUrl(this);
+                brAPIService = new BrAPIService(brapiBaseURL, new DataHelper(BrapiTrialActivity.this));
+
+                TextView baseURLText = findViewById(R.id.brapiBaseURL);
+                baseURLText.setText(brapiBaseURL);
+
+                loadToolbar();
+                loadTrials();
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.brapi_must_configure_url, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.device_offline_warning, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(null);
+            getSupportActionBar().getThemedContext();
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+    }
+
+    private void loadTrials() {
+        ListView trialsView = findViewById(R.id.brapiTrials);
+        trialsView.setVisibility(View.GONE);
+        findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
+
+        String programDbId = getIntent().getStringExtra(BrapiActivity.PROGRAM_DB_ID_INTENT_PARAM);
+
+        brAPIService.getTrials(BrAPIService.getBrapiToken(this), programDbId, new Function<List<BrapiTrial>, Void>() {
+            @Override
+            public Void apply(List<BrapiTrial> trials) {
+                (BrapiTrialActivity.this).runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        trialsView.setAdapter(BrapiTrialActivity.this.buildTrialsArrayAdapter(trials));
+                        trialsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                brapiTrial = trials.get(position);
+                            }
+                        });
+                        trialsView.setVisibility(View.VISIBLE);
+                        findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+                    }
+                });
+                return null;
+            }
+        }, new Function<String, Void>() {
+            @Override
+            public Void apply(String input) {
+                (BrapiTrialActivity.this).runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Show error message. We don't finish the activity intentionally.
+                        findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+                        Toast.makeText(getApplicationContext(), input, Toast.LENGTH_LONG).show();
+                    }
+                });
+                return null;
+            }
+        });
+    }
+
+    private ListAdapter buildTrialsArrayAdapter(List<BrapiTrial> trials) {
+        List<Object> itemDataList = new ArrayList<>();
+        for (BrapiTrial trial : trials) {
+            itemDataList.add(trial.getTrialName());
+        }
+        ListAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, itemDataList);
+        return adapter;
+    }
+
+    public void buttonClicked(View view) {
+       switch (view.getId()) {
+           case R.id.loadTrials:
+               loadTrials();
+               break;
+           case R.id.selectTrial:
+               Intent intent = new Intent();
+               intent.setData(Uri.parse(this.brapiTrial.getTrialDbId()));
+               setResult(RESULT_OK, intent);
+               finish();
+               break;
+       }
+    }
+}
