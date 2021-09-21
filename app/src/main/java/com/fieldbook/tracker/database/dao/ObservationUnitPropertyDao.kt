e8fca91478a992c4db84f684b22991e997a589e4
chaneylc <chaneylc@gmail.com>
2021-09-21 11:24:42 -0500

    Added fix for table export when column names had spaces.

--- app/src/main/java/com/fieldbook/tracker/database/dao/ObservationUnitPropertyDao.kt
+++ app/src/main/java/com/fieldbook/tracker/database/dao/ObservationUnitPropertyDao.kt
@@ -208,7 +208,7 @@ class ObservationUnitPropertyDao {
         fun convertDatabaseToTable(uniqueName: String, col: Array<String?>, traits: Array<String>): Cursor? = withDatabase { db ->
 
             val sanitizeTraits = traits.map { DataHelper.replaceIdentifiers(it) }
-            val select = col.joinToString(",") { "props.$it" }
+            val select = col.joinToString(",") { "props.'${DataHelper.replaceIdentifiers(it)}'" }
 
             val maxStatements = arrayListOf<String>()
             sanitizeTraits.forEach {
@@ -221,7 +221,7 @@ class ObservationUnitPropertyDao {
                 SELECT $select,
                 ${maxStatements.joinToString(",\n")}
                 FROM ObservationUnitProperty as props
-                LEFT JOIN observations o ON props.${uniqueName} = o.observation_unit_id
+                LEFT JOIN observations o ON props.`${uniqueName}` = o.observation_unit_id
                 GROUP BY props.id
             """.trimIndent()
 
