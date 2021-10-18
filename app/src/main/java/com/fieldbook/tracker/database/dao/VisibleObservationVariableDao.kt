112e9ea7e843a1fcfc7f5905609cf7a7c9892800
chaneylc <chaneylc@gmail.com>
2021-10-18 14:51:58 -0500

    cleaned up getDetail query code moved preference to brapi preferences

--- app/src/main/java/com/fieldbook/tracker/database/dao/VisibleObservationVariableDao.kt
+++ app/src/main/java/com/fieldbook/tracker/database/dao/VisibleObservationVariableDao.kt
@@ -67,61 +67,21 @@ class VisibleObservationVariableDao {
         fun getDetail(trait: String): TraitObject? = withDatabase { db ->
 
             //return a trait object but requires multiple queries to use the attr/values table.
-            //TODO temporarily commented out until a database migration can be made to handle additionalInfo
-//            TraitObject().apply {
-//                //creates local scoping around the map result from querying for variable names
-//                with(db.query(sVisibleObservationVariableViewName,
-//                        select = arrayOf(ObservationVariable.PK,
-//                                "observation_variable_name",
-//                                "observation_variable_field_book_format",
-//                                "observation_variable_details",
-//                                "default_value"),
-//                        where = "observation_variable_name LIKE ?",
-//                        whereArgs = arrayOf(trait)).toFirst()) {
-//                    //use the local scoping to initialize trait object fields
-//                    setTrait(this["observation_variable_name"] as? String ?: "")
-//                    format = this["observation_variable_field_book_format"] as? String ?: ""
-//                    defaultValue = this["default_value"] as? String ?: ""
-//                    details = this["observation_variable_details"] as? String ?: ""
-//                    id = (this[ObservationVariable.PK] as? Int ?: -1).toString()
-//                    externalDbId = this["external_db_id"] as? String ?: ""
-//                    additionalInfo = this["additional_info"] as? String ?: ""
-//
-//                    minimum = ""
-//                    maximum = ""
-//                    categories = ""
+            ObservationVariableDao.getAllTraitObjects().first { it.trait == trait }.apply {
+                ObservationVariableValueDao.getVariableValues(id.toInt()).also { values ->
 
+                    values?.forEach {
 
-                ObservationVariableDao.getAllTraitObjects().first { it.trait == trait }.apply {
-                    ObservationVariableValueDao.getVariableValues(id.toInt()).also { values ->
+                        val attrName = ObservationVariableAttributeDao.getAttributeNameById(it[ObservationVariableAttribute.FK] as Int)
 
-                        values?.forEach {
-
-                            //println(it)
-
-//                            println(ObservationVariableValueModel.getAll()?.map {
-//                                "${it[ObservationVariableAttributeModel.FK]} -> ${it["observation_variable_attribute_value"]}"
-//                            })
-//                            println(ObservationVariableAttributeModel.getAll()?.map { it["observation_variable_attribute_name"] })
-
-                            val attrName = ObservationVariableAttributeDao.getAttributeNameById(it[ObservationVariableAttribute.FK] as Int)
-
-//                            println(attrName)
-
-                            when (attrName) {
-                                "validValuesMin" -> minimum = it["observation_variable_attribute_value"] as? String ?: ""
-                                "validValuesMax" -> maximum = it["observation_variable_attribute_value"] as? String ?: ""
-                                "category" -> categories = it["observation_variable_attribute_value"] as? String ?: ""
-                            }
-
-//                            println(it["observation_variable_attribute_value"])
+                        when (attrName) {
+                            "validValuesMin" -> minimum = it["observation_variable_attribute_value"] as? String ?: ""
+                            "validValuesMax" -> maximum = it["observation_variable_attribute_value"] as? String ?: ""
+                            "category" -> categories = it["observation_variable_attribute_value"] as? String ?: ""
                         }
 
-//                        minimum = values!!["validValuesMin"] as? String ?: ""
-//                        maximum = values["validValuesMax"] as? String ?: ""
-//                        categories = values["category"] as? String ?: ""
                     }
-//                }
+                }
             }
         }
     }
