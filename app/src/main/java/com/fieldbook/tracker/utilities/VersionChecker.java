e1a78c4fa18209cb15a054c9fc61331165748461
bellerbrock <bje24@cornell.edu>
2023-06-20 12:24:45 -0400

    undo accidental edit

--- app/src/main/java/com/fieldbook/tracker/utilities/VersionChecker.java
+++ app/src/main/java/com/fieldbook/tracker/utilities/VersionChecker.java
@@ -1,7 +1,7 @@
 package com.fieldbook.tracker.utilities;
 
 import android.app.AlertDialog;
-import android.content.Context;git add -u
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
