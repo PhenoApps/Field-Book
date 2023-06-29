72da89304efcd8d4409dd860f471d570e6e97a16
chaneylc <chaneylc@gmail.com>
2023-06-29 16:00:01 -0500

    more translatable texts

--- app/src/main/java/com/fieldbook/tracker/utilities/VersionChecker.java
+++ app/src/main/java/com/fieldbook/tracker/utilities/VersionChecker.java
@@ -110,7 +110,7 @@ public class VersionChecker extends AsyncTask<Void, Void, String> {
                                 dialog.dismiss();
                             }
                         });
-                        builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 dialog.dismiss();
