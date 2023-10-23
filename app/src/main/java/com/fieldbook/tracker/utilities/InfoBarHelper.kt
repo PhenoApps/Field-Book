855042bdce3897f606bdfc94eca5f085d6f80eb9
bellerbrock <bje24@cornell.edu>
2023-10-23 17:35:28 -0400

    improve infobar defaults

--- app/src/main/java/com/fieldbook/tracker/utilities/InfoBarHelper.kt
+++ app/src/main/java/com/fieldbook/tracker/utilities/InfoBarHelper.kt
@@ -38,8 +38,24 @@ class InfoBarHelper @Inject constructor(@ActivityContext private val context: Co
      */
     fun getInfoBarData(): ArrayList<InfoBarModel> {
 
+        var database : DataHelper = (context as CollectActivity).getDatabase();
+
         //get the preference number of infobars to load
         val numInfoBars: Int = ep.getInt(GeneralKeys.INFOBAR_NUMBER, 2)
+
+        //get all plot attribute names for the study
+        val attributes: List<String> = ArrayList(Arrays.asList(*database.rangeColumnNames))
+
+        //get all traits for this study
+        val traits = database.allTraitObjects
+
+        //create a new array with just trait names
+        val traitNames = ArrayList<String>()
+        if (traits != null) {
+            for (t in traits) {
+                traitNames.add(t.trait)
+            }
+        }
 
         //initialize a list of infobar models that will be served to the adapter
         val infoBarModels = ArrayList<InfoBarModel>()
@@ -47,44 +63,22 @@ class InfoBarHelper @Inject constructor(@ActivityContext private val context: Co
         //iterate and build the arraylist
         for (i in 0 until numInfoBars) {
 
-            var database : DataHelper = (context as CollectActivity).getDatabase();
-
-            //ensure that the initialLabel is actually a plot attribute
-
-            //get all plot attribute names for the study
-            val attributes: List<String> = ArrayList(Arrays.asList(*database.rangeColumnNames))
-
-            //get all traits for this study
-            val traits = database.allTraitObjects
-
-            //create a new array with just trait names
-            val traitNames = ArrayList<String>()
-            if (traits != null) {
-                for (t in traits) {
-                    traitNames.add(t.trait)
-                }
-            }
-
-            //get the default label for the infobar using 'Select' (used in original adapter code)
-            //      or the first item in the attributes list
-            var defaultLabel = "Select"
-            if (attributes.size > 0) {
-                defaultLabel = attributes[0]
-            }
-
-            //get the preferred infobar label, default to above if it doesn't exist for this position
-            //adapter preferred values are saved as DROP1, DROP2, DROP3, DROP4, DROP5 in preferences, intiailize the label with it
-            var initialLabel: String = ep.getString("DROP$i", defaultLabel) ?: defaultLabel
+            //get the preferred infobar label, default to "Select" if it doesn't exist for this position
+            //adapter preferred values are saved as DROP1, DROP2, DROP3, DROP4, DROP5 in preferences, initialize the label with it
+            var initialLabel: String = ep.getString("DROP$i", "Select") ?: "Select"
 
             //check if the label is an attribute or a trait, this will decide how to query the database for the value
-            val isAttribute = attributes.contains(initialLabel)
+            var isAttribute = attributes.contains(initialLabel)
 
             //check if the label actually exists in the attributes/traits (this will reset on field switch)
-            //if it doesn't exist, default to the first attribute in the list
+            //if it doesn't exist, default to the next attribute in the list
             if (!isAttribute) {
                 if (!traitNames.contains(initialLabel)) {
-                    if (attributes.isNotEmpty()) {
-                        initialLabel = attributes[0]
+                    if (i in attributes.indices) {
+                        initialLabel = attributes[i]
+                        isAttribute = true
+                    } else {
+                        initialLabel = "Select"
                     }
                 }
             }
