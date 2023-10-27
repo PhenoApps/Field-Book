69d5ea8b0b12c5dde847799fd65f54f3eeda7e2c
trife <twrife@clemson.edu>
2023-10-27 06:39:16 -0400

    icon and popup

--- app/src/main/java/com/fieldbook/tracker/preferences/BetaPreferencesFragment.kt
+++ app/src/main/java/com/fieldbook/tracker/preferences/BetaPreferencesFragment.kt
@@ -35,18 +35,6 @@ class BetaPreferencesFragment : PreferenceFragmentCompat() {
 
         val barcode = findPreference<CheckBoxPreference>(GeneralKeys.MLKIT_PREFERENCE_KEY)
         barcode?.setOnPreferenceChangeListener { _, newValue ->
-
-            if (newValue == true) {
-
-                if (isAdded) {
-                    AlertDialog.Builder(context)
-                            .setTitle(getString(R.string.pref_beta_mlkit_enabled_title))
-                            .setMessage(getString(R.string.pref_beta_mlkit_enabled_message))
-                            .setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.dismiss() }
-                            .show()
-                }
-            }
-
             true
         }
     }
