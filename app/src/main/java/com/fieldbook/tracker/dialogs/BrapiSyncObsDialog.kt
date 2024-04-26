d9b2008a7e003dade8fff6d700c163828bc90a0b
Prasad Kamath <kamathprasad9@gmail.com>
2024-04-26 01:28:19 -0400

    BrAPI Sync Field dialog

--- app/src/main/java/com/fieldbook/tracker/dialogs/BrapiSyncObsDialog.kt
+++ app/src/main/java/com/fieldbook/tracker/dialogs/BrapiSyncObsDialog.kt
@@ -11,8 +11,10 @@ import android.os.Bundle
 import android.os.Handler
 import android.text.Html
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 import androidx.preference.PreferenceManager
@@ -36,16 +38,18 @@ data class StudyObservations(var fieldBookStudyDbId:Int=0, val traitList: Mutabl
     }
 }
 
-class BrapiSyncObsDialog(context: Context, private val syncController: FieldSyncController, private val fieldObject: FieldObject ) : Dialog(context), View.OnClickListener {
+class BrapiSyncObsDialog(context: Context, private val syncController: FieldSyncController, private val fieldObject: FieldObject ) : Dialog(context) {
     private var saveBtn: Button? = null
     private var noObsLabel: TextView? = null
     private var brAPIService: BrAPIService? = null
 
     private var studyObservations = StudyObservations()
 
+    private var dialogBrAPISyncObs: AlertDialog? = null
+
     lateinit var paginationManager: BrapiPaginationManager
 
-    lateinit var fieldNameLbl: TextView
+    private var fieldNameLbl: TextView? = null
 
     // Creates a new thread to do importing
     private val importRunnable =
@@ -53,45 +57,50 @@ class BrapiSyncObsDialog(context: Context, private val syncController: FieldSync
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setCanceledOnTouchOutside(false)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        setContentView(R.layout.dialog_brapi_sync_observations)
+        showDialog()
         setupUI()
     }
+
+    private fun showDialog() {
+        val builder = AlertDialog.Builder(context, R.style.AppAlertDialog)
+            .setTitle(R.string.brapi_obs_header_name)
+            .setNegativeButton(R.string.dialog_cancel) { d, _ ->
+                d.dismiss()
+            }
+            .setPositiveButton(R.string.dialog_save) { _, _ ->
+                saveObservations()
+            }
+
+        val view = layoutInflater.inflate(R.layout.dialog_brapi_sync_observations, null)
+        builder.setView(view)
+        dialogBrAPISyncObs = builder.create()
+        dialogBrAPISyncObs?.show()
+
+        saveBtn = dialogBrAPISyncObs?.getButton(AlertDialog.BUTTON_POSITIVE)
+    }
+
 
     private fun setupUI() {
         brAPIService = BrAPIServiceFactory.getBrAPIService(this.context)
         val pageSize = PreferenceManager.getDefaultSharedPreferences(context)
             .getString(GeneralKeys.BRAPI_PAGE_SIZE, "50")!!.toInt()
         paginationManager = BrapiPaginationManager(0, pageSize)
-        saveBtn = findViewById(R.id.brapi_save_btn)
-        saveBtn!!.setOnClickListener(this)
-        noObsLabel = findViewById(R.id.noObservationLbl)
-        fieldNameLbl = findViewById(R.id.studyNameValue)
-        val cancelBtn = findViewById<Button>(R.id.brapi_cancel_btn)
-        cancelBtn.setOnClickListener(this)
+        saveBtn = dialogBrAPISyncObs?.getButton(AlertDialog.BUTTON_POSITIVE)
+        noObsLabel = dialogBrAPISyncObs?.findViewById(R.id.noObservationLbl)
+        fieldNameLbl = dialogBrAPISyncObs?.findViewById(R.id.studyNameValue)
     }
 
     override fun onStart() {
         // Set our OK button to be disabled until we are finished loading
-        saveBtn!!.visibility = View.GONE
+        saveBtn?.visibility = View.GONE
         //Hide the error message in case no observations are downloaded.
-        noObsLabel!!.visibility = View.GONE
+        noObsLabel?.visibility = View.GONE
 
-        fieldNameLbl.text = fieldObject.exp_name
+        fieldNameLbl?.text = fieldObject.exp_name
 
         //need to call the load code
         loadObservations()
-    }
-
-    override fun onClick(v: View) {
-        println("Clicked: ${v.id}")
-        when (v.id) {
-            R.id.brapi_save_btn -> saveObservations()
-            R.id.brapi_cancel_btn -> dismiss()
-            else -> {}
-        }
-        dismiss()
     }
 
     /**
@@ -100,7 +109,7 @@ class BrapiSyncObsDialog(context: Context, private val syncController: FieldSync
     private fun loadObservations() {
         val brapiStudyDbId = fieldObject.study_db_id
         val fieldBookStudyDbId = fieldObject.exp_id
-        Log.d("BrapiSyncObsDialog", "brapiStudyDbId is "+brapiStudyDbId+" and fieldBookStudyDbId is "+fieldBookStudyDbId);
+        Log.d("BrapiSyncObsDialog", "brapiStudyDbId is $brapiStudyDbId and fieldBookStudyDbId is $fieldBookStudyDbId")
         val brAPIService = BrAPIServiceFactory.getBrAPIService(this.context)
 
 
@@ -174,19 +183,19 @@ class BrapiSyncObsDialog(context: Context, private val syncController: FieldSync
      * Function that first populates the trait and observation counts, hides the loading spinner and
      * then will pop up the Save button.
      */
-    fun makeSaveBtnVisible() {
+    private fun makeSaveBtnVisible() {
         populateCountsAndHideLoadingPanel()
 
-        saveBtn!!.visibility = View.VISIBLE
+        saveBtn?.visibility = View.VISIBLE
     }
 
     /**
      * Function that first populates the trait and observation counts, hides the loading spinner and
      * then will pop up the Warning message if no observations are downloaded.
      */
-    fun makeNoObsWarningVisible() {
+    private fun makeNoObsWarningVisible() {
         populateCountsAndHideLoadingPanel()
-        noObsLabel!!.visibility = View.VISIBLE
+        noObsLabel?.visibility = View.VISIBLE
     }
 
     /**
@@ -194,15 +203,15 @@ class BrapiSyncObsDialog(context: Context, private val syncController: FieldSync
      */
     private fun populateCountsAndHideLoadingPanel() {
         //populate the description fields
-        (findViewById<View>(R.id.numberTraitsValue) as TextView).text =
+        (dialogBrAPISyncObs?.findViewById<View>(R.id.numberTraitsValue) as TextView).text =
             "${studyObservations.traitList.size}"
-        (findViewById<View>(R.id.numberObsValue) as TextView).text =
+        (dialogBrAPISyncObs?.findViewById<View>(R.id.numberObsValue) as TextView).text =
             "${studyObservations.observationList.size}"
 
-        findViewById<View>(R.id.loadingPanel).visibility = View.GONE
+        dialogBrAPISyncObs?.findViewById<View>(R.id.loadingPanel)?.visibility = View.GONE
     }
 
-    fun saveObservations() {
+    private fun saveObservations() {
         // Dismiss this dialog
         dismiss()
 
@@ -214,20 +223,20 @@ class BrapiSyncObsDialog(context: Context, private val syncController: FieldSync
 // Mimics the class used in the csv field importer to run the saving
 // task in a different thread from the UI thread so the app doesn't freeze up.
 internal class ImportRunnableTask(
-    val context: Context, val studyObservations: StudyObservations,
+    val context: Context, private val studyObservations: StudyObservations,
     private val syncController: FieldSyncController) : AsyncTask<Int, Int, Int>() {
 
     var dialog: ProgressDialog? = null
     var fail = false
-    var failMessage = ""
+    private var failMessage = ""
 
     override fun onPreExecute() {
         super.onPreExecute()
         dialog = ProgressDialog(context)
-        dialog!!.isIndeterminate = true
-        dialog!!.setCancelable(false)
-        dialog!!.setMessage(Html.fromHtml(context.resources.getString(R.string.import_dialog_importing)))
-        dialog!!.show()
+        dialog?.isIndeterminate = true
+        dialog?.setCancelable(false)
+        dialog?.setMessage(Html.fromHtml(context.resources.getString(R.string.import_dialog_importing)))
+        dialog?.show()
     }
 
     override fun doInBackground(vararg params: Int?): Int? {
@@ -288,7 +297,7 @@ internal class ImportRunnableTask(
         if (result == null || fail) {
             val alertDialogBuilder = AlertDialog.Builder(context, R.style.AppAlertDialog)
             alertDialogBuilder.setTitle(R.string.dialog_save_error_title)
-                .setPositiveButton(R.string.dialog_ok) { dialogInterface, i ->
+                .setPositiveButton(R.string.dialog_ok) { _, _ ->
                     // Finish our BrAPI import activity
                     (context as Activity).finish()
                 }
@@ -296,7 +305,7 @@ internal class ImportRunnableTask(
             val alertDialog = alertDialogBuilder.create()
             alertDialog.show()
         } else {
-            syncController?.onSyncComplete()
+            syncController.onSyncComplete()
         }
     }
 }
