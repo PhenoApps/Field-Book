7b5dc0e1c196f7d4d478c4184e38fc98e92cf4f7
kamathprasad9 <54414375+kamathprasad9@users.noreply.github.com>
2024-07-19 13:13:04 -0400

    Change background, text colors

--- app/src/main/java/com/fieldbook/tracker/activities/AppIntroActivity.kt
+++ app/src/main/java/com/fieldbook/tracker/activities/AppIntroActivity.kt
@@ -3,10 +3,12 @@ package com.fieldbook.tracker.activities
 import android.Manifest
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceManager
 import com.fieldbook.tracker.R
@@ -18,6 +20,7 @@ import com.fieldbook.tracker.preferences.GeneralKeys
 import com.fieldbook.tracker.utilities.Constants
 import com.github.appintro.AppIntro
 import com.github.appintro.AppIntroFragment
+import com.github.appintro.AppIntroFragment.Companion.createInstance
 import dagger.hilt.android.AndroidEntryPoint
 import org.phenoapps.utils.BaseDocumentTreeUtil.Companion.getRoot
 import pub.devrel.easypermissions.EasyPermissions
@@ -40,21 +43,25 @@ class AppIntroActivity : AppIntro() {
 
         // field book info 1
         addSlide(
-            AppIntroFragment.createInstance(
+            createInstance(
                 context.getString(R.string.app_intro_intro_title_slide1),
                 context.getString(R.string.app_intro_intro_summary_slide1),
                 R.drawable.field_book_intro,
-                R.color.blue_primary
+                R.color.main_primary_transparent,
+                R.color.main_color_text_dark,
+                R.color.main_color_text_dark,
             )
         )
 
         // field book info 2
         addSlide(
-            AppIntroFragment.createInstance(
+            createInstance(
                 context.getString(R.string.app_intro_intro_title_slide2),
                 context.getString(R.string.app_intro_intro_summary_slide2),
                 R.drawable.field_book_intro,
-                R.color.blue_primary_transparent
+                R.color.main_primary_transparent,
+                R.color.main_color_text_dark,
+                R.color.main_color_text_dark,
             )
         )
 
@@ -77,6 +84,12 @@ class AppIntroActivity : AppIntro() {
         )
 
         isSkipButtonEnabled = false
+
+        setNextArrowColor(ContextCompat.getColor(this, R.color.main_primary_dark))
+        setIndicatorColor(
+            ContextCompat.getColor(this, R.color.main_primary_dark),
+            ContextCompat.getColor(this, R.color.main_primary_transparent)
+        )
     }
 
     override fun onDonePressed(currentFragment: Fragment?) {
