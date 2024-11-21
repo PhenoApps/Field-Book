c4a70bc044165b4252ea87519abbed8850117a25
chaneylc <chaneylc@gmail.com>
2024-11-21 13:19:58 -0600

    added string resource for "Up" action improved performance

--- app/src/main/java/com/fieldbook/tracker/activities/FileExploreActivity.kt
+++ app/src/main/java/com/fieldbook/tracker/activities/FileExploreActivity.kt
@@ -3,14 +3,12 @@ package com.fieldbook.tracker.activities
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
-import android.widget.ListAdapter
 import android.widget.ListView
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -22,40 +20,48 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.phenoapps.utils.BaseDocumentTreeUtil
-import org.phenoapps.utils.BaseDocumentTreeUtil.Companion.getStem
-import java.util.Arrays
 import java.util.Locale
 
 class FileExploreActivity : ActivityDialog(), CoroutineScope by MainScope() {
+
+    companion object {
+        const val EXTRA_RESULT_KEY: String =
+            "com.fieldbook.tracker.activities.FieldEditorActivity.extras.RESULT"
+    }
+
     private var mainListView: ListView? = null
     private var progressBar: ProgressBar? = null
 
     // Stores names of traversed directories
     private val str = ArrayList<String?>()
-    private val mComparator = java.util.Comparator { f1: DocumentFile, f2: DocumentFile ->
-        if (f1.isDirectory && !f2.isDirectory) {
+
+    // Check if the first level of the directory structure is the one showing
+    private var firstLvl = true
+    private var fileList = mutableListOf<Item>()
+    private var chosenFile: String? = null
+
+    //updates whenever we traverse a directory, loads the current level's files
+    private var path: DocumentFile? = null
+
+    // Wrapper class to hold file data
+    private class Item(var file: String, var isDir: Boolean, var icon: Int) {
+        override fun toString(): String {
+            return file
+        }
+    }
+
+    private val mComparator = java.util.Comparator { f1: Item, f2: Item ->
+        if (f1.isDir && !f2.isDir) {
             // Directory before non-directory
             return@Comparator -1
-        } else if (!f1.isDirectory && f2.isDirectory) {
+        } else if (!f1.isDir && f2.isDir) {
             // Non-directory after directory
             return@Comparator 1
         } else {
             // Alphabetic order otherwise
-            val f1Name: String = f1.uri.getStem(this)
-            val f2Name: String = f2.uri.getStem(this)
-            return@Comparator f1Name.compareTo(f2Name, ignoreCase = true)
+            return@Comparator f1.file.compareTo(f2.file, ignoreCase = true)
         }
     }
-
-    // Check if the first level of the directory structure is the one showing
-    private var firstLvl = true
-    private var fileList: Array<Item?>? = null
-    private var chosenFile: String? = null
-    private var adapter: ListAdapter? = null
-
-    //updates whenever we traverse a directory, loads the current level's files
-    private var path: DocumentFile? = null
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,10 +80,19 @@ class FileExploreActivity : ActivityDialog(), CoroutineScope by MainScope() {
 
                 val constraintLayout = ConstraintLayout(this)
 
-                constraintLayout.addView(mainListView, ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT)
-                constraintLayout.addView(progressBar, ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT)
+                constraintLayout.addView(
+                    mainListView,
+                    ConstraintLayout.LayoutParams.MATCH_PARENT,
+                    ConstraintLayout.LayoutParams.MATCH_PARENT
+                )
+                constraintLayout.addView(
+                    progressBar,
+                    ConstraintLayout.LayoutParams.MATCH_PARENT,
+                    ConstraintLayout.LayoutParams.MATCH_PARENT
+                )
 
-                val pbSize = resources.getDimension(R.dimen.act_file_explorer_progress_bar_size).toInt()
+                val pbSize =
+                    resources.getDimension(R.dimen.act_file_explorer_progress_bar_size).toInt()
 
                 progressBar?.isIndeterminate = true
                 progressBar?.layoutParams = ConstraintLayout.LayoutParams(pbSize, pbSize).also {
@@ -102,7 +117,7 @@ class FileExploreActivity : ActivityDialog(), CoroutineScope by MainScope() {
 
                 mainListView?.onItemClickListener =
                     AdapterView.OnItemClickListener { _: AdapterView<*>?, _: View?, which: Int, _: Long ->
-                        chosenFile = fileList?.get(which)?.file
+                        chosenFile = fileList[which].file
                         //File sel = new File(path + "/" + chosenFile);
                         val file = path?.findFile(chosenFile!!)
                         if (file != null && file.exists() && file.isDirectory) {
@@ -110,31 +125,30 @@ class FileExploreActivity : ActivityDialog(), CoroutineScope by MainScope() {
 
                             // Adds chosen directory to list
                             str.add(chosenFile)
-                            fileList = null
+                            fileList.clear()
                             path = file
 
                             loadFilesProgress(file, exclude, include)
 
-                            mainListView?.adapter = adapter
-                        } else if (chosenFile.equals("up", ignoreCase = true)
+                        } else if (chosenFile.equals(getString(R.string.activity_file_explorer_up_directory_name))
                             && (file == null || !file.exists())
                         ) {
                             // present directory removed from list
-
-                            val s = str.removeAt(str.size - 1)
+                            str.removeAt(str.size - 1)
 
                             // path modified to exclude present directory
                             path = path?.parentFile
 
-                            fileList = null
+                            fileList.clear()
 
                             // if there are no more directories in the list, then
                             // its the first level
                             if (str.isEmpty()) {
                                 firstLvl = true
                             }
+
                             loadFilesProgress(path, exclude, include)
-                            mainListView?.adapter = adapter
+
                         } else {
                             try {
                                 if (file != null && file.exists()) {
@@ -149,14 +163,51 @@ class FileExploreActivity : ActivityDialog(), CoroutineScope by MainScope() {
                         }
                     }
             }
+
         } else {
+
             setResult(RESULT_CANCELED)
 
             finish()
         }
     }
 
-    private fun loadFilesProgress(path: DocumentFile?, exclude: Array<String>? = null, include: Array<String>? = null) {
+    private fun updateAdapter() {
+        mainListView?.adapter = object : ArrayAdapter<Item?>(
+            this,
+            R.layout.custom_dialog_item_select, android.R.id.text1,
+            fileList.toTypedArray()
+        ) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                // creates view
+                val view = super.getView(position, convertView, parent)
+
+                if (fileList.isNotEmpty()) {
+
+                    view.findViewById<TextView>(android.R.id.text1).also { tv ->
+
+                        // put the image on the text view
+                        tv.setCompoundDrawablesWithIntrinsicBounds(
+                            fileList[position].icon, 0, 0, 0
+                        )
+
+                        // add margin between image and text (support various screen
+                        // densities)
+                        val dp5 = (5 * resources.displayMetrics.density + 0.5f).toInt()
+                        tv.compoundDrawablePadding = dp5
+                    }
+                }
+
+                return view
+            }
+        }
+    }
+
+    private fun loadFilesProgress(
+        path: DocumentFile?,
+        exclude: Array<String>? = null,
+        include: Array<String>? = null
+    ) {
 
         progressBar?.visibility = View.VISIBLE
         mainListView?.visibility = View.INVISIBLE
@@ -172,7 +223,7 @@ class FileExploreActivity : ActivityDialog(), CoroutineScope by MainScope() {
             withContext(Dispatchers.Main) {
                 progressBar?.visibility = View.GONE
                 mainListView?.visibility = View.VISIBLE
-                mainListView?.adapter = adapter
+                updateAdapter()
             }
         }
     }
@@ -182,16 +233,10 @@ class FileExploreActivity : ActivityDialog(), CoroutineScope by MainScope() {
         exclude: Array<String>?,
         include: Array<String>?
     ) {
-        // Checks whether path exists
-
         if (path != null && path.exists()) {
+
             //list all files in the passed directory
-
             val files = path.listFiles()
-
-            //created an 'accepted' list of the files that do not have the excluded (param) extension
-            //and do contain the included extensions
-            val accepted = ArrayList<DocumentFile>()
 
             //populate exclude/include lists before iteration intent extra is optional and can be null
             val excludedExtensions = if (exclude == null) ArrayList()
@@ -213,87 +258,42 @@ class FileExploreActivity : ActivityDialog(), CoroutineScope by MainScope() {
                     if (excludedExtensions.contains(checkExtension(name))) continue
 
                     if (includedExtensions.contains(checkExtension(name)) || file.isDirectory) {
-                        accepted.add(file)
+                        val nextIndex = fileList.map { it.file }.binarySearch(name)
+                        val index = if (nextIndex >= 0) nextIndex else -(nextIndex + 1)
+
+                        val uiItem = Item(name, file.isDirectory, R.drawable.ic_file_generic)
+
+                        if (file.exists()) {
+
+                            // Set drawables
+                            if (file.isDirectory) {
+                                uiItem.icon = R.drawable.ic_file_directory
+                            } else if (name.lowercase(Locale.getDefault()).contains(".csv")) {
+                                uiItem.icon = R.drawable.ic_file_csv
+                            } else if (name.contains(".xls")) {
+                                uiItem.icon = R.drawable.ic_file_xls
+                            }
+
+                            fileList.add(index, uiItem)
+
+                        }
+
                         continue
                     }
                 }
             }
 
-            Arrays.sort(files, mComparator)
-
-            // collect file names
-            val fileNameList = arrayOfNulls<String>(accepted.size)
-            for (file in accepted) {
-                fileNameList[accepted.indexOf(file)] = file.name
-            }
-
-            // create file list for
-            fileList = arrayOfNulls(fileNameList.size)
-
-            for (i in fileNameList.indices) {
-                fileList!![i] = Item(
-                    fileNameList[i]!!, R.drawable.ic_file_generic
-                )
-
-                val file = path.findFile(fileNameList[i]!!)
-
-                if (file != null && file.exists()) {
-                    val name = file.name
-
-                    if (name != null) {
-                        // Set drawables
-
-                        if (file.isDirectory) {
-                            fileList!![i]!!.icon = R.drawable.ic_file_directory
-                            Log.d("DIRECTORY", fileList!![i]!!.file)
-                        }
-                        if (name.lowercase(Locale.getDefault()).contains(".csv")) {
-                            fileList!![i]!!.icon = R.drawable.ic_file_csv
-                        }
-
-                        if (name.contains(".xls")) {
-                            fileList!![i]!!.icon = R.drawable.ic_file_xls
-                        } else {
-                            Log.d("FILE", fileList!![i]!!.file)
-                        }
-                    }
-                }
-            }
+            fileList.sortWith(mComparator)
 
             if (!firstLvl) {
-                val temp = arrayOfNulls<Item>(
-                    fileList!!.size + 1
-                )
-                System.arraycopy(fileList, 0, temp, 1, fileList!!.size)
-                temp[0] = Item("Up", R.drawable.ic_file_up_dir)
-                fileList = temp
-            }
-        }
-
-        adapter = object : ArrayAdapter<Item?>(
-            this,
-            R.layout.custom_dialog_item_select, android.R.id.text1,
-            fileList!!
-        ) {
-            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-                // creates view
-                val view = super.getView(position, convertView, parent)
-                val textView = view.findViewById<TextView>(android.R.id.text1)
-
-                fileList?.get(position)?.let {
-                    // put the image on the text view
-                    textView.setCompoundDrawablesWithIntrinsicBounds(
-                        it.icon, 0, 0, 0
+                fileList.add(
+                    0,
+                    Item(
+                        getString(R.string.activity_file_explorer_up_directory_name),
+                        true,
+                        R.drawable.ic_file_up_dir
                     )
-                }
-
-
-                // add margin between image and text (support various screen
-                // densities)
-                val dp5 = (5 * resources.displayMetrics.density + 0.5f).toInt()
-                textView.compoundDrawablePadding = dp5
-
-                return view
+                )
             }
         }
     }
@@ -314,17 +314,5 @@ class FileExploreActivity : ActivityDialog(), CoroutineScope by MainScope() {
         }
 
         return super.onOptionsItemSelected(item)
-    }
-
-    // Wrapper class to hold file data
-    private class Item(var file: String, var icon: Int) {
-        override fun toString(): String {
-            return file
-        }
-    }
-
-    companion object {
-        const val EXTRA_RESULT_KEY: String =
-            "com.fieldbook.tracker.activities.FieldEditorActivity.extras.RESULT"
     }
 }
