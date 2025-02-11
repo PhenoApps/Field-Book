20a43096ced78b3ef4224423da0f37dcc4b02344
kamathprasad9 <54414375+kamathprasad9@users.noreply.github.com>
2025-02-10 19:03:15 -0500

    Refactor required setup

--- app/src/main/java/com/fieldbook/tracker/activities/AppIntroActivity.kt
+++ app/src/main/java/com/fieldbook/tracker/activities/AppIntroActivity.kt
@@ -1,33 +1,21 @@
 package com.fieldbook.tracker.activities
 
-import android.Manifest
-import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.fieldbook.tracker.R
 import com.fieldbook.tracker.adapters.OptionalSetupAdapter
-import com.fieldbook.tracker.adapters.RequiredSetupAdapter
 import com.fieldbook.tracker.fragments.GallerySlideFragment
 import com.fieldbook.tracker.fragments.OptionalSetupPolicyFragment
 import com.fieldbook.tracker.fragments.RequiredSetupPolicyFragment
 import com.fieldbook.tracker.preferences.GeneralKeys
-import com.fieldbook.tracker.utilities.Constants
 import com.fieldbook.tracker.utilities.ManufacturerUtil
 import com.fieldbook.tracker.utilities.SharedPreferenceUtils
 import com.github.appintro.AppIntro
 import com.github.appintro.AppIntroFragment.Companion.createInstance
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.runBlocking
-import org.phenoapps.fragments.storage.PhenoLibStorageDefinerFragment.AssetSample
-import org.phenoapps.utils.BaseDocumentTreeUtil
-import org.phenoapps.utils.BaseDocumentTreeUtil.Companion.getRoot
-import pub.devrel.easypermissions.EasyPermissions
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -36,73 +24,14 @@ class AppIntroActivity : AppIntro() {
     @Inject
     lateinit var prefs: SharedPreferences
 
-    private var directories: Array<String>? = null
-
-    private var samples = mapOf(
-        AssetSample("field_import", "field_sample.csv") to R.string.dir_field_import,
-        AssetSample("field_import", "field_sample2.csv") to R.string.dir_field_import,
-        AssetSample("field_import", "field_sample3.csv") to R.string.dir_field_import,
-        AssetSample("field_import", "rtk_sample.csv") to R.string.dir_field_import,
-        AssetSample("field_import", "training_sample.csv") to R.string.dir_field_import,
-        AssetSample("resources", "feekes_sample.jpg") to R.string.dir_resources,
-        AssetSample("resources", "stem_rust_sample.jpg") to R.string.dir_resources,
-        AssetSample("trait", "trait_sample.trt") to R.string.dir_trait,
-        AssetSample("trait", "severity.txt") to R.string.dir_trait,
-        AssetSample("database", "sample_db.zip") to R.string.dir_database)
-
-    private val launcher = registerForActivityResult(ActivityResultContracts.OpenDocumentTree()) { uri ->
-
-        uri?.let { nonNullUri ->
-
-            val flags =
-                Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
-          
-            this@AppIntroActivity.contentResolver.takePersistableUriPermission(nonNullUri, flags)
-            runBlocking {
-
-                directories?.let { dirs ->
-
-                    BaseDocumentTreeUtil.defineRootStructure(this@AppIntroActivity, nonNullUri, dirs)?.let { root ->
-
-                        samples.entries.forEach { entry ->
-
-                            val sampleAsset = entry.key
-                            val dir = entry.value
-
-                            BaseDocumentTreeUtil.copyAsset(this@AppIntroActivity, sampleAsset.name, sampleAsset.dir, dir)
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-
-        val context = applicationContext
-
-        //define directories that should be created in root storage
-        context?.let { ctx ->
-            val archive = ctx.getString(R.string.dir_archive)
-            val db = ctx.getString(R.string.dir_database)
-            val fieldExport = ctx.getString(R.string.dir_field_export)
-            val fieldImport = ctx.getString(R.string.dir_field_import)
-            val geonav = ctx.getString(R.string.dir_geonav)
-            val plotData = ctx.getString(R.string.dir_plot_data)
-            val resources = ctx.getString(R.string.dir_resources)
-            val trait = ctx.getString(R.string.dir_trait)
-            val updates = ctx.getString(R.string.dir_updates)
-            directories = arrayOf(archive, db, fieldExport, fieldImport,
-                geonav, plotData, resources, trait, updates
-            )
-        }
+        super.onCreate(savedInstanceState)
 
         // field book info 1
         addSlide(
             createInstance(
-                context.getString(R.string.app_intro_intro_title_slide1),
-                context.getString(R.string.app_intro_intro_summary_slide1),
+                getString(R.string.app_intro_intro_title_slide1),
+                getString(R.string.app_intro_intro_summary_slide1),
                 R.drawable.field_book_intro_icon,
                 R.color.main_primary_transparent,
                 R.color.main_color_text_dark,
@@ -116,9 +45,9 @@ class AppIntroActivity : AppIntro() {
         // required setup slide
         addSlide(
             RequiredSetupPolicyFragment.newInstance(
-                requiredSetupModelArrayList(),
-                context.getString(R.string.app_intro_required_setup_title),
-                context.getString(R.string.app_intro_required_setup_summary)
+                getString(R.string.app_intro_required_setup_title),
+                getString(R.string.app_intro_required_setup_summary),
+                R.color.main_primary_transparent
             )
         )
 
@@ -126,8 +55,8 @@ class AppIntroActivity : AppIntro() {
         addSlide(
             OptionalSetupPolicyFragment.newInstance(
                 optionalSetupModelArrayList(),
-                context.getString(R.string.app_intro_required_optional_title),
-                context.getString(R.string.app_intro_required_optional_summary)
+                getString(R.string.app_intro_required_optional_title),
+                getString(R.string.app_intro_required_optional_summary)
             )
         )
 
@@ -145,67 +74,6 @@ class AppIntroActivity : AppIntro() {
         super.onDonePressed(currentFragment)
         setResult(RESULT_OK)
         finish()
-    }
-
-    private fun requiredSetupModelArrayList(): ArrayList<RequiredSetupAdapter.RequiredSetupModel> {
-        var perms = arrayOf<String?>(
-            Manifest.permission.VIBRATE,
-            Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.CAMERA
-        )
-        val finePerms = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-        val coarsePerms =
-            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            perms = arrayOf(
-                Manifest.permission.VIBRATE,
-                Manifest.permission.RECORD_AUDIO,
-                Manifest.permission.CAMERA
-            )
-        }
-
-        return arrayListOf(
-            RequiredSetupAdapter.RequiredSetupModel(
-                applicationContext.getString(R.string.app_intro_permissions_title),
-                applicationContext.getString(R.string.permission_rationale_trait_features),
-                resources.getDrawable(R.drawable.configure),
-                {
-                    ActivityCompat.requestPermissions(
-                        this,
-                        Constants.permissions,
-                        Constants.PERM_REQ
-                    )
-                },
-                {
-                    return@RequiredSetupModel (EasyPermissions.hasPermissions(
-                        this,
-                        *perms
-                    )
-                            && (EasyPermissions.hasPermissions(
-                        this,
-                        *finePerms
-                    ) || EasyPermissions.hasPermissions(this, *coarsePerms)))
-                },
-                applicationContext.getString(R.string.app_intro_permissions_warning)
-            ),
-            RequiredSetupAdapter.RequiredSetupModel(
-                applicationContext.getString(R.string.app_intro_storage_title),
-                applicationContext.getString(R.string.app_intro_storage_summary),
-                resources.getDrawable(R.drawable.storage_lock),
-                {
-                    launcher.launch(null)
-                },
-
-                {
-                    val root = getRoot(applicationContext)
-                    return@RequiredSetupModel (root != null && root.exists())
-                },
-
-                applicationContext.getString(R.string.app_intro_storage_warning)
-            )
-        )
     }
 
     private fun optionalSetupModelArrayList(): ArrayList<OptionalSetupAdapter.OptionalSetupModel> {
--- app/src/main/java/com/fieldbook/tracker/fragments/RequiredSetupPolicyFragment.kt
+++ app/src/main/java/com/fieldbook/tracker/fragments/RequiredSetupPolicyFragment.kt
@@ -1,35 +1,98 @@
 package com.fieldbook.tracker.fragments
 
+import android.Manifest
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
+import androidx.preference.PreferenceManager
 import com.fieldbook.tracker.R
-import com.fieldbook.tracker.adapters.RequiredSetupAdapter
-import com.fieldbook.tracker.adapters.RequiredSetupAdapter.RequiredSetupModel
+import com.fieldbook.tracker.utilities.Constants
+import com.fieldbook.tracker.utilities.Utils
+import com.fieldbook.tracker.views.RequiredSetupItem
 import com.github.appintro.SlidePolicy
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.phenoapps.fragments.storage.PhenoLibStorageDefinerFragment.AssetSample
+import org.phenoapps.utils.BaseDocumentTreeUtil
+import org.phenoapps.utils.BaseDocumentTreeUtil.Companion.getRoot
+import pub.devrel.easypermissions.EasyPermissions
 
 
 class RequiredSetupPolicyFragment : Fragment(), SlidePolicy {
-
-    private var recyclerView: RecyclerView? = null
-    private var setupItems: List<RequiredSetupModel>? = null
     private var slideTitle: String? = null
     private var slideSummary: String? = null
+    private var slideBackgroundColor: Int? = null
+
+    private var permissionsSetupItem: RequiredSetupItem? = null
+    private var storageDefinerSetupItem: RequiredSetupItem? = null
+
+    private var prefs: SharedPreferences? = null
+
+    private val scope by lazy { CoroutineScope(Dispatchers.Main) }
+
+    private var directories: Array<String>? = null
+
+    private var samples = mapOf(
+        AssetSample("field_import", "field_sample.csv") to R.string.dir_field_import,
+        AssetSample("field_import", "field_sample2.csv") to R.string.dir_field_import,
+        AssetSample("field_import", "field_sample3.csv") to R.string.dir_field_import,
+        AssetSample("field_import", "rtk_sample.csv") to R.string.dir_field_import,
+        AssetSample("field_import", "training_sample.csv") to R.string.dir_field_import,
+        AssetSample("resources", "feekes_sample.jpg") to R.string.dir_resources,
+        AssetSample("resources", "stem_rust_sample.jpg") to R.string.dir_resources,
+        AssetSample("trait", "trait_sample.trt") to R.string.dir_trait,
+        AssetSample("trait", "severity.txt") to R.string.dir_trait,
+        AssetSample("database", "sample_db.zip") to R.string.dir_database)
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.OpenDocumentTree()) { uri ->
+
+        uri?.let { nonNullUri ->
+
+            val flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+
+            context?.contentResolver?.takePersistableUriPermission(nonNullUri, flags)
+            runBlocking {
+
+                directories?.let { dirs ->
+
+                    BaseDocumentTreeUtil.defineRootStructure(context, nonNullUri, dirs)?.let { _ ->
+
+                        samples.entries.forEach { entry ->
+
+                            val sampleAsset = entry.key
+                            val dir = entry.value
+
+                            BaseDocumentTreeUtil.copyAsset(context, sampleAsset.name, sampleAsset.dir, dir)
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.app_intro_setup_slide, container, false)
+    ): View? = inflater.inflate(R.layout.app_intro_required_setup_slide, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        prefs = context?.let { PreferenceManager.getDefaultSharedPreferences(it) }
 
         val slideTitle = view.findViewById<TextView>(R.id.slide_title)
         val slideSummary = view.findViewById<TextView>(R.id.slide_summary)
@@ -37,31 +100,119 @@ class RequiredSetupPolicyFragment : Fragment(), SlidePolicy {
         slideTitle?.text = this.slideTitle
         slideSummary?.text = this.slideSummary
 
+        slideBackgroundColor?.let { view.setBackgroundResource(it) }
 
-        recyclerView = view.findViewById(R.id.setup_rv)
-        recyclerView?.adapter = RequiredSetupAdapter()
+        permissionsSetupItem = view.findViewById(R.id.permissions_setup_item)
+        storageDefinerSetupItem = view.findViewById(R.id.storage_definer_setup_item)
 
-        setupItems?.let {
-            (recyclerView?.adapter as? RequiredSetupAdapter)?.submitList(it)
+        initSetupItems()
+
+        //define directories that should be created in root storage
+        context?.let { ctx ->
+            val archive = ctx.getString(R.string.dir_archive)
+            val db = ctx.getString(R.string.dir_database)
+            val fieldExport = ctx.getString(R.string.dir_field_export)
+            val fieldImport = ctx.getString(R.string.dir_field_import)
+            val geonav = ctx.getString(R.string.dir_geonav)
+            val plotData = ctx.getString(R.string.dir_plot_data)
+            val resources = ctx.getString(R.string.dir_resources)
+            val trait = ctx.getString(R.string.dir_trait)
+            val updates = ctx.getString(R.string.dir_updates)
+            directories = arrayOf(archive, db, fieldExport, fieldImport,
+                geonav, plotData, resources, trait, updates
+            )
         }
     }
 
-    private fun validateItems(): Boolean {
-        setupItems?.forEach { item ->
-            if (!item.isSet()) {
-                return false
+    private fun initSetupItems() {
+        permissionsSetupItem?.apply {
+            setIcon(R.drawable.configure)
+            setTitle(getString(R.string.app_intro_permissions_title))
+            setSummary(getString(R.string.permission_rationale_trait_features))
+            setOnClickListener {
+                performSetup(this, {requestPermissions()}, {validatePermissions()})
             }
         }
-        return true
+
+        storageDefinerSetupItem?.apply {
+            setIcon(R.drawable.storage_lock)
+            setTitle(getString(R.string.app_intro_storage_title))
+            setSummary(getString(R.string.app_intro_storage_summary))
+            setOnClickListener {
+                performSetup(this, {requestStorageDefiner()}, {validateStorage()})
+            }
+        }
+
+        checkSetupStatus(permissionsSetupItem, validatePermissions())
+        checkSetupStatus(storageDefinerSetupItem, validateStorage())
     }
 
-    private fun getFirstInvalidItem(): Int {
-        setupItems?.forEachIndexed { index, item ->
-            if (!item.isSet()) {
-                return index
+    private fun performSetup(requiredSetupItem: RequiredSetupItem, setupLaunch: () -> Unit, validatorFunction: () -> Boolean) {
+        scope.launch {
+
+            setupLaunch()
+
+            withContext(Dispatchers.Default) {
+                while (!validatorFunction()) {
+                    // check every 100ms if the item is set up i.e. call back is completed
+                    delay(100)
+                }
             }
+
+            checkSetupStatus(requiredSetupItem, validatorFunction())
+
         }
-        return -1
+    }
+
+    private fun checkSetupStatus(setupItemView: RequiredSetupItem?, isSet: Boolean) {
+        if (isSet) {
+            setupItemView?.setStatus(R.drawable.ic_about_up_to_date)
+        }
+    }
+
+    private fun requestPermissions() {
+        activity?.let { ActivityCompat.requestPermissions(it, Constants.permissions, Constants.PERM_REQ) }
+    }
+
+    private fun requestStorageDefiner() {
+        launcher.launch(null)
+    }
+
+    private fun validatePermissions(): Boolean {
+        var permissionsGranted = false
+        var perms = arrayOf<String?>(
+            Manifest.permission.VIBRATE,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        )
+        val finePerms = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+        val coarsePerms =
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            perms = arrayOf(
+                Manifest.permission.VIBRATE,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA
+            )
+        }
+
+        context?.let {
+            permissionsGranted =
+                (EasyPermissions.hasPermissions(it, *perms)
+                && (
+                    EasyPermissions.hasPermissions(it, *finePerms) || EasyPermissions.hasPermissions(it, *coarsePerms)
+                )
+            )
+        }
+
+        return permissionsGranted
+    }
+
+    private fun validateStorage(): Boolean {
+        val root = getRoot(context)
+        return (root != null && root.exists())
     }
 
     override val isPolicyRespected: Boolean
@@ -69,29 +220,32 @@ class RequiredSetupPolicyFragment : Fragment(), SlidePolicy {
 
     override fun onUserIllegallyRequestedNextPage() {
         try {
-            Toast.makeText(
-                requireContext().applicationContext,
-                setupItems?.get(getFirstInvalidItem())?.invalidateMessage,
-                Toast.LENGTH_SHORT
-            ).show()
+            if (!validatePermissions()) {
+                Utils.makeToast(context, getString(R.string.app_intro_permissions_warning))
+            } else if (!validateStorage()) {
+                Utils.makeToast(context, getString(R.string.app_intro_storage_warning))
+            }
         } catch (e: Exception) {
             e.printStackTrace()
             FirebaseCrashlytics.getInstance().recordException(e)
         }
     }
 
+    private fun validateItems(): Boolean {
+        return validatePermissions() && validateStorage()
+    }
+
     companion object {
         fun newInstance(
-            setupItems: ArrayList<RequiredSetupModel>,
             slideTitle: String,
-            slideSummary: String
+            slideSummary: String,
+            slideBackgroundColor: Int
         ): RequiredSetupPolicyFragment {
             val fragment = RequiredSetupPolicyFragment()
-            fragment.setupItems = setupItems
             fragment.slideTitle = slideTitle
             fragment.slideSummary = slideSummary
+            fragment.slideBackgroundColor = slideBackgroundColor
             return fragment
         }
-
     }
 }
