25048c3c28d897de8a6de9aca7e8670947a5afcc
kamathprasad9 <54414375+kamathprasad9@users.noreply.github.com>
2025-03-07 16:07:57 -0500

    Change compass to semicircular

--- app/src/main/java/com/fieldbook/tracker/views/CompassView.kt
+++ app/src/main/java/com/fieldbook/tracker/views/CompassView.kt
@@ -9,6 +9,7 @@ import android.util.AttributeSet
 import android.view.View
 import kotlin.math.cos
 import kotlin.math.sin
+import androidx.core.graphics.withRotation
 
 class CompassView @JvmOverloads constructor(
     context: Context,
@@ -24,13 +25,13 @@ class CompassView @JvmOverloads constructor(
     }
 
     // use prevAngle to avoid resetting to 0f everytime
-    private var currentYawAngle = prevAngle
-    private var size = 0
+    private var currentAngle = prevAngle
+    private var boxWidth = 0
 
     private val needleHeadPath = Path()
     private val needleTailPath = Path()
 
-    private val circle = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    private val semiCircle = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
         strokeWidth = CIRCLE_THICKNESS
         color = Color.BLACK
@@ -59,15 +60,17 @@ class CompassView @JvmOverloads constructor(
 
     // calculates the size of the compass
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        size = minOf(
-            MeasureSpec.getSize(widthMeasureSpec),
-            MeasureSpec.getSize(heightMeasureSpec)
-        )
-        setMeasuredDimension(size, size)
+        boxWidth = MeasureSpec.getSize(widthMeasureSpec)
+
+        val radius = (boxWidth / 2f) - 10f
+        val tailLength = radius * 0.3f
+
+        val height = boxWidth/2 + tailLength.toInt() + 20 // 50dp padding below
+        setMeasuredDimension(boxWidth, height)
     }
 
     fun setYawAngle(angle: Float) {
-        currentYawAngle = angle
+        currentAngle = angle
         prevAngle = angle
         invalidate()
     }
@@ -78,21 +81,34 @@ class CompassView @JvmOverloads constructor(
         needleHeadPath.reset()
         needleTailPath.reset()
 
-        val centerX = size / 2f
-        val centerY = size / 2f
-        val radius = (size / 2f) - 10f // 10f is for margin
+        val centerX = boxWidth / 2f
+        val centerY = boxWidth / 2f
+        val radius = (boxWidth / 2f) - 10f // 10f is for margin
 
         // compass circle
-        canvas.drawCircle(centerX, centerY, radius, circle)
+        val startAngle = 180f
+        val sweepAngle = 180f
+        canvas.drawArc(
+            centerX - radius,
+            centerY - radius,
+            centerX + radius,
+            centerY + radius,
+            startAngle,
+            sweepAngle,
+            false,
+            semiCircle
+        )
 
         angleText.textSize = radius / 8f
 
-        for (angle in 0 until 360 step ANGLE_INTERVAL) {
-            val radian = Math.toRadians((angle + 180).toDouble()).toFloat()
+        for (angle in -90..90 step ANGLE_INTERVAL) {
+            val adjustedAngle = angle - 90
+
+            val radian = Math.toRadians(adjustedAngle.toDouble()).toFloat()
 
             /*
                 x = centerX + cos(theta) * r
-                y = centerY = sin(theta) * r
+                y = centerY + sin(theta) * r
              */
 
             // angle markers
@@ -106,36 +122,33 @@ class CompassView @JvmOverloads constructor(
             // angle texts
             val textX = centerX + cos(radian) * (radius - 40)
             val heightOfText = (angleText.descent() - angleText.ascent())
-            val textY = centerY + sin(radian) * (radius - 40) +  heightOfText/ 2
+            val textY = centerY + sin(radian) * (radius - 40) + heightOfText / 2
             canvas.drawText(angle.toString(), textX, textY, angleText)
         }
 
-        canvas.save()
-        canvas.rotate(currentYawAngle-90, centerX, centerY)
-
         // needle
-        val headLength = radius * 0.8f
-        val tailLength = radius * 0.3f
-        val arrowWidth = radius * 0.15f
+        canvas.withRotation(currentAngle, centerX, centerY) {
+            val headLength = radius * 0.8f
+            val tailLength = radius * 0.3f
+            val arrowWidth = radius * 0.15f
 
-        // needleHead
-        needleHeadPath.apply {
-            moveTo(centerX, centerY - headLength)
-            lineTo(centerX - arrowWidth, centerY)
-            lineTo(centerX + arrowWidth, centerY)
-            close()
-            canvas.drawPath(this, needleHead)
+            // needleHead
+            needleHeadPath.apply {
+                moveTo(centerX, centerY - headLength)
+                lineTo(centerX - arrowWidth, centerY)
+                lineTo(centerX + arrowWidth, centerY)
+                close()
+                drawPath(this, needleHead)
+            }
+
+            // needleTail
+            needleTailPath.apply {
+                moveTo(centerX, centerY + tailLength)
+                lineTo(centerX - arrowWidth, centerY)
+                lineTo(centerX + arrowWidth, centerY)
+                close()
+                drawPath(this, needleTail)
+            }
         }
-
-        // needleTail
-        needleTailPath.apply {
-            moveTo(centerX, centerY + tailLength)
-            lineTo(centerX - arrowWidth, centerY)
-            lineTo(centerX + arrowWidth, centerY)
-            close()
-            canvas.drawPath(this, needleTail)
-        }
-
-        canvas.restore()
     }
 }
