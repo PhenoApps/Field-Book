3db7c6dda76b10e01ca339dee0d35b704fcccafc
trife <twrife@clemson.edu>
2025-07-01 14:11:47 -0400

    remove additional innospectra references

--- app/src/main/java/com/fieldbook/tracker/traits/formats/InnoSpectraSensorFormat.kt
+++ app/src/main/java/com/fieldbook/tracker/traits/formats/InnoSpectraSensorFormat.kt
@@ -3,7 +3,7 @@ package com.fieldbook.tracker.traits.formats
 import com.fieldbook.tracker.R
 
 open class InnoSpectraSensorFormat: BaseSpectralFormat(
-    format = Formats.INNO_SPECTRA,
+    //format = Formats.INNO_SPECTRA,
     databaseName = "inno_spectra",
     nameStringResourceId = R.string.traits_format_inno_spectra,
     iconDrawableResourceId = R.drawable.lightbulb,
