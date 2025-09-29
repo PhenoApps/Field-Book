db118d6132f9a835342e9a3a927c037606866c4e
Nahuel Soldevilla <n.soldevilla@integratedbreeding.net>
2025-09-29 15:32:45 -0300

    IBP-7142 test with no resources

--- shared/src/commonMain/kotlin/com/fieldbook/shared/ConfigScreen.kt
+++ shared/src/commonMain/kotlin/com/fieldbook/shared/ConfigScreen.kt
@@ -4,33 +4,23 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.fieldbook.shared.generated.resources.Res
-import com.fieldbook.shared.generated.resources.ic_nav_drawer_collect_data
-import com.fieldbook.shared.generated.resources.ic_nav_drawer_fields
-import com.fieldbook.shared.generated.resources.ic_nav_drawer_settings
-import com.fieldbook.shared.generated.resources.ic_nav_drawer_statistics
-import com.fieldbook.shared.generated.resources.ic_nav_drawer_traits
-import com.fieldbook.shared.generated.resources.ic_tb_info
-import com.fieldbook.shared.generated.resources.trait_date_save
 import com.fieldbook.shared.theme.MainTheme
-import org.jetbrains.compose.resources.painterResource
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -46,7 +36,7 @@ fun ConfigScreen(onBack: (() -> Unit)? = null) {
             "Statistics",
             "About"
         )
-        val configIcons = listOf(
+        /*val configIcons = listOf(
             Res.drawable.ic_nav_drawer_fields,
             Res.drawable.ic_nav_drawer_traits,
             Res.drawable.ic_nav_drawer_collect_data,
@@ -54,7 +44,7 @@ fun ConfigScreen(onBack: (() -> Unit)? = null) {
             Res.drawable.ic_nav_drawer_settings,
             Res.drawable.ic_nav_drawer_statistics,
             Res.drawable.ic_tb_info
-        )
+        )*/
         Surface(modifier = Modifier.fillMaxSize()) {
             androidx.compose.foundation.layout.Column(modifier = Modifier.fillMaxSize()) {
                 TopAppBar(
@@ -76,18 +66,18 @@ fun ConfigScreen(onBack: (() -> Unit)? = null) {
                     )
                 )
                 LazyColumn(modifier = Modifier.fillMaxSize()) {
-                    itemsIndexed(configItems ) { index, item ->
+                    itemsIndexed(configItems) { index, item ->
                         Row(
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(16.dp),
                             verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Icon(
+                            /*Icon(
                                 painter = painterResource(configIcons[index]),
                                 contentDescription = item,
                                 modifier = Modifier.padding(end = 16.dp).size(24.dp)
-                            )
+                            )*/
                             Text(
                                 text = item,
                                 style = MaterialTheme.typography.bodyLarge
