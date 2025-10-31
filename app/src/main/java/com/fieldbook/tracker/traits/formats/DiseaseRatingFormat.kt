9e94716bc345ce12469394fdead942c8b7aa0bde
kamathprasad9 <54414375+kamathprasad9@users.noreply.github.com>
2025-10-31 16:50:11 -0400

    Fix dbName for disease rating format and sample files

--- app/src/main/java/com/fieldbook/tracker/traits/formats/DiseaseRatingFormat.kt
+++ app/src/main/java/com/fieldbook/tracker/traits/formats/DiseaseRatingFormat.kt
@@ -10,7 +10,7 @@ class DiseaseRatingFormat : TraitFormat(
     format = Formats.DISEASE_RATING,
     defaultLayoutId = R.layout.trait_disease_rating,
     layoutView = null,
-    databaseName = "rust rating",
+    databaseName = "disease rating",
     nameStringResourceId = R.string.traits_format_disease_rating,
     iconDrawableResourceId = R.drawable.ic_trait_disease_rating,
     stringNameAux = null,
