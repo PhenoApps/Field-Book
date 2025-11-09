f1d188e492ee41e518950a0abbf1c328519e8980
kamathprasad9 <54414375+kamathprasad9@users.noreply.github.com>
2025-11-09 16:06:48 -0500

    Segregate colors depending on use, apply to material theme

--- app/src/main/java/com/fieldbook/tracker/ui/theme/AppTheme.kt
+++ app/src/main/java/com/fieldbook/tracker/ui/theme/AppTheme.kt
@@ -7,13 +7,16 @@ import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.preference.PreferenceManager
 import com.fieldbook.tracker.preferences.PreferenceKeys
-import com.fieldbook.tracker.ui.theme.colors.BlueColors
-import com.fieldbook.tracker.ui.theme.colors.DefaultColors
-import com.fieldbook.tracker.ui.theme.colors.HighContrastColors
-import com.fieldbook.tracker.ui.theme.colors.ThemeColors
+import com.fieldbook.tracker.ui.theme.colors.AppColors
+import com.fieldbook.tracker.ui.theme.colors.BlueAppColors
+import com.fieldbook.tracker.ui.theme.colors.DefaultAppColors
+import com.fieldbook.tracker.ui.theme.colors.HighContrastAppColors
+import com.fieldbook.tracker.ui.theme.enums.AppTextType
+import com.fieldbook.tracker.ui.theme.enums.AppThemeType
 import com.fieldbook.tracker.ui.theme.typography.CompactTypography
 import com.fieldbook.tracker.ui.theme.typography.ExpandedTypography
 import com.fieldbook.tracker.ui.theme.typography.MediumTypography
@@ -27,7 +30,9 @@ import com.fieldbook.tracker.ui.theme.typography.ThemeTypography
  *                  Box()
  *             }
  *      - use colors/text sizes
- *          eg. AppTheme.colors.primary, AppTheme.typography.bodyStyle
+ *          eg. AppTheme.colors.primary, AppTheme.colors.dataVisualization.heatmap.high, AppTheme.typography.bodyStyle
+ *
+ *  TODO: add darkTheme support
  */
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
@@ -39,37 +44,59 @@ fun AppTheme(
     val prefs = PreferenceManager.getDefaultSharedPreferences(context)
 
     val themeIndex = prefs.getString(PreferenceKeys.THEME, "0")?.toInt() ?: 0
+    val themeType = when(themeIndex) {
+        0 -> AppThemeType.Default
+        1 -> AppThemeType.HighContrast
+        2 -> AppThemeType.Blue
+        else -> AppThemeType.Default
+    }
+
     val textIndex = prefs.getString(PreferenceKeys.TEXT_THEME, "1")?.toInt() ?: 1
+    val textType = AppTextType.entries.find { it.index == textIndex } ?: AppTextType.MEDIUM
+
+    // select colors based on theme type
+    val colors = remember(themeType) {
+        when(themeType) {
+            AppThemeType.Default -> DefaultAppColors
+            AppThemeType.HighContrast -> HighContrastAppColors
+            AppThemeType.Blue -> BlueAppColors
+        }
+    }
+
     val windowSizeClass = activity?.let { calculateWindowSizeClass(it) }
 
-    val colors = when(themeIndex) {
-        0 -> DefaultColors
-        1 -> HighContrastColors
-        2 -> BlueColors
-        else -> DefaultColors
+    // select typography based on window size and text size preference
+    val typography = remember(windowSizeClass, textType) {
+        when (windowSizeClass?.widthSizeClass) {
+            WindowWidthSizeClass.Compact -> CompactTypography.all[textType.index]
+            WindowWidthSizeClass.Medium -> MediumTypography.all[textType.index]
+            WindowWidthSizeClass.Expanded -> ExpandedTypography.all[textType.index]
+            else -> CompactTypography.all[textType.index]
+        }
     }
 
-    val typography = when (windowSizeClass?.widthSizeClass) {
-        WindowWidthSizeClass.Compact -> CompactTypography.all[textIndex]
-        WindowWidthSizeClass.Medium -> MediumTypography.all[textIndex]
-        WindowWidthSizeClass.Expanded -> ExpandedTypography.all[textIndex]
-        else -> CompactTypography.all[textIndex]
-    }
+    val materialColorScheme = remember(colors) { colors.toMaterialColorScheme() }
 
-    // enable theme/text related values available to composables
+    val materialTypography = remember(typography) { typography.toMaterialTypography() }
+
+    // provide both custom colors and typography
     CompositionLocalProvider(
         LocalAppColors provides colors,
         LocalAppTypography provides typography
     ) {
         MaterialTheme(
+            colorScheme = materialColorScheme,
+            typography = materialTypography,
             content = content
         )
     }
 }
 
-// Convenience object for accessing theme values
+/**
+ * Convenience object for accessing theme values
+ */
 object AppTheme {
-    val colors: ThemeColors
+    val colors: AppColors
         @Composable get() = LocalAppColors.current
 
     val typography: ThemeTypography
---
+++ app/src/main/java/com/fieldbook/tracker/ui/theme/colors/ColorDefinition.kt
@@ -0,0 +1,268 @@
+package com.fieldbook.tracker.ui.theme.colors
+
+/**
+ * Define theme colors for default theme, and override specific colors for other theme in their own implementation
+ */
+val DefaultAppColors = AppColors(
+    primary = BaseColors.Primary,
+    primaryDark = BaseColors.PrimaryDark,
+    accent = BaseColors.Accent,
+    primaryTransparent = BaseColors.PrimaryTransparent,
+    background = BaseColors.Background,
+
+    surface = SurfaceColors(
+        border = BaseColors.Border,
+        preferencesHorizontalBreak = BaseColors.PreferencesHorizontalBreak,
+        iconTint = BaseColors.IconTint,
+        iconFillTint = BaseColors.IconFillTint
+    ),
+
+    text = TextColors(
+        primary = BaseColors.TextLight,
+        secondary = BaseColors.TextSecondary,
+        tertiary = BaseColors.TextDark,
+        hint = BaseColors.HintText,
+        highContrast = BaseColors.TextHighContrastInverted,
+        title = BaseColors.TextLight,
+        subheading = BaseColors.SubheadingColor,
+        button = BaseColors.ButtonText,
+    ),
+
+    button = ButtonColors(
+        normal = BaseColors.ButtonColorNormal,
+        pressed = BaseColors.ButtonColorPressed,
+        textBackground = BaseColors.TextButtonBackground,
+        categoricalPress = BaseColors.CategoricalButtonPress,
+        categoricalSelected = BaseColors.CategoricalButtonSelected,
+        traitBackground = BaseColors.TraitButtonBackgroundTint,
+    ),
+
+    interactive = InteractiveColors(
+        tapTarget = BaseColors.TapTarget,
+        spinnerSelected = BaseColors.SpinnerSelected,
+        spinnerFocused = BaseColors.SpinnerFocused,
+        seekBar = BaseColors.SeekbarColor,
+        seekBarThumb = BaseColors.SeekbarThumb,
+        selectedItemBackground = BaseColors.SelectedItemBackground,
+    ),
+
+    status = StatusColors(
+        valueSaved = BaseColors.ValueSaved,
+        valueAltered = BaseColors.ValueAltered,
+        success = BaseColors.PrimaryDark,
+        error = BaseColors.ErrorMessage,
+        bluetoothConnected = BaseColors.BluetoothConnected,
+        progressBar = BaseColors.PrimaryDark,
+    ),
+
+    dataVisualization = DataVisualizationColors(
+        heatmap = HeatmapColors(
+            low = BaseColors.HeatmapLow,
+            medium = BaseColors.HeatmapMedium,
+            high = BaseColors.HeatmapHigh,
+            max = BaseColors.HeatmapMax,
+        ),
+        graph = GraphColors(
+            itemSelected = BaseColors.GraphItemSelected,
+            itemUnselected = BaseColors.GraphItemUnselected,
+            itemText = BaseColors.GraphItemText,
+        ),
+        dataGrid = DataGridColors(
+            emptyCell = BaseColors.EmptyCell,
+            activeCell = BaseColors.ActiveCell,
+            tableBorder = BaseColors.TableBorder,
+            dataFilled = BaseColors.DataFilled,
+            cellText = BaseColors.CellText,
+            activeCellText = BaseColors.ActiveCellText,
+        )
+    ),
+
+    trait = TraitColors(
+        percent = PercentTraitColors(
+            backgroundCenter = BaseColors.TraitPercentBackgroundCenter,
+            backgroundStartEnd = BaseColors.TraitPercentBackgroundStartEnd,
+            stroke = BaseColors.TraitPercentStroke,
+            start = BaseColors.TraitPercentStart
+        ),
+        boolean = BooleanTraitColors(
+            `true` = BaseColors.BooleanTrue,
+            `false` = BaseColors.BooleanFalse
+        )
+    ),
+
+    chip = ChipColors(
+        defaultBackground = BaseColors.DefaultChipBackground,
+        selectableBackground = BaseColors.SelectableChipBackground,
+        selectableStroke = BaseColors.SelectableChipStroke,
+        first = BaseColors.ChipFirst,
+        second = BaseColors.ChipSecond,
+        third = BaseColors.ChipThird,
+        fourth = BaseColors.ChipThird
+    ),
+
+    stepper = StepperColors(
+        icon = BaseColors.StepperIcon,
+        iconBackground = BaseColors.StepperIconBg,
+        iconOnDone = BaseColors.StepperIconOnDone,
+        iconOnDoneBackground = BaseColors.StepperIconOnDoneBg,
+        line = BaseColors.StepperLine,
+        lineOnDone = BaseColors.StepperLineOnDone
+    ),
+
+    crop = CropColors(
+        inverseRegion = BaseColors.InverseCropRegion
+    ),
+)
+
+val BlueAppColors = DefaultAppColors.copy(
+    primary = BlueThemeOverrides.Primary,
+    primaryDark = BlueThemeOverrides.PrimaryDark,
+    accent = BlueThemeOverrides.Accent,
+    primaryTransparent = BlueThemeOverrides.PrimaryTransparent,
+
+    surface = DefaultAppColors.surface.copy(
+        iconFillTint = BlueThemeOverrides.IconFillTint,
+    ),
+
+    button = DefaultAppColors.button.copy(
+        pressed = BlueThemeOverrides.ButtonColorPressed,
+        categoricalSelected = BlueThemeOverrides.CategoricalButtonSelected
+    ),
+
+    interactive = DefaultAppColors.interactive.copy(
+        spinnerFocused = BlueThemeOverrides.SpinnerFocused,
+        spinnerSelected = BlueThemeOverrides.SpinnerSelected,
+        selectedItemBackground = BlueThemeOverrides.SelectedItemBackground
+    ),
+
+    status = DefaultAppColors.status.copy(
+        success = BlueThemeOverrides.PrimaryDark,
+        progressBar = BlueThemeOverrides.PrimaryDark,
+    ),
+
+    dataVisualization = DefaultAppColors.dataVisualization.copy(
+        heatmap = HeatmapColors(
+            low = BlueThemeOverrides.HeatmapLow,
+            medium = BlueThemeOverrides.HeatmapMedium,
+            high = BlueThemeOverrides.HeatmapHigh,
+            max = BlueThemeOverrides.HeatmapMax
+        ),
+        graph = DefaultAppColors.dataVisualization.graph.copy(
+            itemSelected = BlueThemeOverrides.GraphItemSelected,
+        ),
+        dataGrid = DefaultAppColors.dataVisualization.dataGrid.copy(
+            activeCell = BlueThemeOverrides.ActiveCell,
+            dataFilled = BlueThemeOverrides.DataFilled,
+        ),
+    ),
+
+    trait = DefaultAppColors.trait.copy(
+        percent = DefaultAppColors.trait.percent.copy(
+            stroke = BlueThemeOverrides.TraitPercentStroke,
+            start = BlueThemeOverrides.TraitPercentStart
+        )
+    ),
+
+    chip = DefaultAppColors.chip.copy(
+        defaultBackground = BlueThemeOverrides.DefaultChipBackground,
+        selectableStroke = BlueThemeOverrides.SelectableChipStroke,
+        first = BlueThemeOverrides.ChipFirst,
+        second = BlueThemeOverrides.ChipSecond,
+        third = BlueThemeOverrides.ChipThird
+    ),
+
+    stepper = DefaultAppColors.stepper.copy(
+        icon = BlueThemeOverrides.StepperIcon,
+        iconOnDoneBackground = BlueThemeOverrides.StepperIconOnDoneBg,
+        lineOnDone = BlueThemeOverrides.StepperLineOnDone
+    ),
+)
+
+val HighContrastAppColors = DefaultAppColors.copy(
+    primary = HighContrastOverrides.Primary,
+    primaryDark = HighContrastOverrides.PrimaryDark,
+    accent = HighContrastOverrides.Accent,
+    primaryTransparent = HighContrastOverrides.PrimaryTransparent,
+
+    surface = DefaultAppColors.surface.copy(
+        iconFillTint = HighContrastOverrides.IconFillTint,
+        preferencesHorizontalBreak = HighContrastOverrides.PreferencesHorizontalBreak
+    ),
+
+    text = DefaultAppColors.text.copy(
+        highContrast = HighContrastOverrides.TextHighContrastInverted
+    ),
+
+    button = DefaultAppColors.button.copy(
+        normal = HighContrastOverrides.ButtonColorNormal,
+        pressed = HighContrastOverrides.ButtonColorPressed,
+        categoricalPress = HighContrastOverrides.CategoricalButtonPress,
+        categoricalSelected = HighContrastOverrides.CategoricalButtonSelected,
+        traitBackground = HighContrastOverrides.TraitButtonBackgroundTint
+    ),
+
+    interactive = DefaultAppColors.interactive.copy(
+        spinnerFocused = HighContrastOverrides.SpinnerFocused,
+        spinnerSelected = HighContrastOverrides.SpinnerSelected,
+        selectedItemBackground = HighContrastOverrides.SelectedItemBackground
+    ),
+
+    status = DefaultAppColors.status.copy(
+        valueSaved = HighContrastOverrides.ValueSaved,
+        valueAltered = HighContrastOverrides.ValueAltered,
+        success = HighContrastOverrides.PrimaryDark,
+        error = HighContrastOverrides.ErrorMessage,
+        bluetoothConnected = HighContrastOverrides.BluetoothConnected,
+        progressBar = HighContrastOverrides.PrimaryDark,
+    ),
+
+    dataVisualization = DefaultAppColors.dataVisualization.copy(
+        heatmap = HeatmapColors(
+            low = HighContrastOverrides.HeatmapLow,
+            medium = HighContrastOverrides.HeatmapMedium,
+            high = HighContrastOverrides.HeatmapHigh,
+            max = HighContrastOverrides.HeatmapMax
+        ),
+        graph = DefaultAppColors.dataVisualization.graph.copy(
+            itemSelected = HighContrastOverrides.GraphItemSelected,
+            itemUnselected = HighContrastOverrides.GraphItemUnselected,
+            itemText = HighContrastOverrides.GraphItemText,
+        ),
+        dataGrid = DefaultAppColors.dataVisualization.dataGrid.copy(
+            emptyCell = HighContrastOverrides.EmptyCell,
+            activeCell = HighContrastOverrides.ActiveCell,
+            dataFilled = HighContrastOverrides.DataFilled,
+        )
+    ),
+
+    trait = DefaultAppColors.trait.copy(
+        percent = DefaultAppColors.trait.percent.copy(
+            backgroundCenter = HighContrastOverrides.TraitPercentBackgroundCenter,
+            backgroundStartEnd = HighContrastOverrides.TraitPercentBackgroundStartEnd,
+            stroke = HighContrastOverrides.TraitPercentStroke,
+            start = HighContrastOverrides.TraitPercentStart
+        ),
+        boolean = DefaultAppColors.trait.boolean.copy(
+            `true` = HighContrastOverrides.BooleanTrue,
+            `false` = HighContrastOverrides.BooleanFalse
+        )
+    ),
+
+    chip = DefaultAppColors.chip.copy(
+        defaultBackground = HighContrastOverrides.DefaultChipBackground,
+        selectableStroke = HighContrastOverrides.SelectableChipStroke,
+        first = HighContrastOverrides.ChipFirst,
+        second = HighContrastOverrides.ChipSecond,
+        third = HighContrastOverrides.ChipThird,
+        fourth = HighContrastOverrides.ChipThird
+    ),
+
+    stepper = DefaultAppColors.stepper.copy(
+        icon = HighContrastOverrides.StepperIcon,
+        iconBackground = HighContrastOverrides.StepperIconBg,
+        iconOnDone = HighContrastOverrides.StepperIconOnDone,
+        iconOnDoneBackground = HighContrastOverrides.StepperIconOnDoneBg,
+        line = HighContrastOverrides.StepperLine,
+        lineOnDone = HighContrastOverrides.StepperLineOnDone
+    ),
+)
