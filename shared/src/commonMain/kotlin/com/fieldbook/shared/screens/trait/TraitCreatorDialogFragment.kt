64ec93070b123dd0c51a0ef785e34ee664bdb936
Nahuel Soldevilla <n.soldevilla@integratedbreeding.net>
2026-01-09 12:59:47 -0300

    IBP-7142 fix trait dialog padding

--- shared/src/commonMain/kotlin/com/fieldbook/shared/screens/trait/TraitCreatorDialogFragment.kt
+++ shared/src/commonMain/kotlin/com/fieldbook/shared/screens/trait/TraitCreatorDialogFragment.kt
@@ -3,19 +3,23 @@ package com.fieldbook.shared.screens.trait
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -26,6 +30,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.fieldbook.shared.database.models.TraitObject
 import com.fieldbook.shared.traits.Formats
@@ -45,60 +51,71 @@ fun TraitCreatorDialog(
 ) {
     var currentStep by remember { mutableStateOf(TraitCreatorStep.ChooseFormat) }
     var selectedFormat by remember { mutableStateOf<Formats?>(null) }
-    var name by remember { mutableStateOf("") }
-    var details by remember { mutableStateOf("") }
+    var traitName by remember { mutableStateOf("") }
+    var traitDetails by remember { mutableStateOf("") }
 
     when (currentStep) {
         TraitCreatorStep.ChooseFormat -> {
-            AlertDialog(
-                modifier = Modifier.fillMaxWidth(), // TODO not working?
+            Dialog(
                 onDismissRequest = onDismiss,
-                title = { Text("Trait Layout") },
-                text = {
-                    val formats = Formats.supportedFormats()
+                properties = DialogProperties(usePlatformDefaultWidth = false)
+            ) {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        Text("Trait Layout", modifier = Modifier.padding(8.dp))
 
-                    LazyVerticalGrid(
-                        columns = GridCells.Fixed(3),
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                        verticalArrangement = Arrangement.spacedBy(12.dp)
-                    ) {
-                        items(formats) { format ->
-                            Column(
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                                modifier = Modifier
-                                    .padding(8.dp)
-                                    .clickable {
-                                        selectedFormat = format
-                                        currentStep = TraitCreatorStep.NameDetails
-                                    }
-                            ) {
-                                val iconRes = format.getIcon()
-                                Image(
-                                    painter = painterResource(iconRes),
-                                    contentDescription = format.name,
-                                    modifier = Modifier.size(48.dp)
-                                )
-                                Spacer(modifier = Modifier.height(8.dp))
-                                Text(stringResource(format.getTraitFormatDefinition().nameStringResource))
+                        val formats = Formats.supportedFormats()
+
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(3),
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                            verticalArrangement = Arrangement.spacedBy(12.dp)
+                        ) {
+                            items(formats) { format ->
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                        .clickable {
+                                            selectedFormat = format
+                                            currentStep = TraitCreatorStep.NameDetails
+                                        }
+                                ) {
+                                    val iconRes = format.getIcon()
+                                    Image(
+                                        painter = painterResource(iconRes),
+                                        contentDescription = format.name,
+                                        modifier = Modifier.size(48.dp)
+                                    )
+                                    Spacer(modifier = Modifier.height(8.dp))
+                                    Text(stringResource(format.getTraitFormatDefinition().nameStringResource))
+                                }
+                            }
+                        }
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.End
+                        ) {
+                            TextButton(onClick = onDismiss) {
+                                Text("Cancel")
                             }
                         }
                     }
-                },
-                confirmButton = {},
-                dismissButton = {
-                    TextButton(onClick = onDismiss) {
-                        Text("Cancel")
-                    }
                 }
-            )
+            }
         }
 
         TraitCreatorStep.NameDetails -> {
             val traitState = remember {
                 TraitObject().apply {
-                    name = name
-                    details = details
+                    name = traitName
+                    details = traitDetails
                     format = selectedFormat?.databaseName
                     visible = "true"
                     traitDataSource = "local"
@@ -107,63 +124,86 @@ fun TraitCreatorDialog(
 
             var paramError by remember { mutableStateOf("") }
 
-            AlertDialog(
+            Dialog(
                 onDismissRequest = onDismiss,
-                title = {
-                    val title = selectedFormat?.let { stringResource(it.getTraitFormatDefinition().nameStringResource) }
-                    Text("${title ?: ""} Parameters")
-                },
-                text = {
-                    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-                        OutlinedTextField(
-                            value = name,
-                            onValueChange = {
-                                name = it
-                                traitState.name = it
-                            },
-                            label = { Text("Name") },
-                            modifier = Modifier.fillMaxWidth()
-                        )
+                properties = DialogProperties(usePlatformDefaultWidth = false)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(24.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Surface(
+                        modifier = Modifier
+                            .widthIn(min = 280.dp, max = 640.dp),
+                        shape = RoundedCornerShape(12.dp),
+                        tonalElevation = 4.dp
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(12.dp),
+                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            val title =
+                                selectedFormat?.let { stringResource(it.getTraitFormatDefinition().nameStringResource) }
+                            Text("${title ?: ""} Parameters", modifier = Modifier.padding(8.dp))
 
-                        OutlinedTextField(
-                            value = details,
-                            onValueChange = {
-                                details = it
-                                traitState.details = it
-                            },
-                            label = { Text("Details") },
-                            modifier = Modifier.fillMaxWidth()
-                        )
+                            OutlinedTextField(
+                                value = traitName,
+                                onValueChange = {
+                                    traitName = it
+                                    traitState.name = it
+                                },
+                                label = { Text("Name") },
+                                modifier = Modifier.fillMaxWidth()
+                            )
 
-                        Spacer(modifier = Modifier.height(8.dp))
+                            OutlinedTextField(
+                                value = traitDetails,
+                                onValueChange = {
+                                    traitDetails = it
+                                    traitState.details = it
+                                },
+                                label = { Text("Details") },
+                                modifier = Modifier.fillMaxWidth()
+                            )
 
-                        selectedFormat?.getTraitFormatDefinition()?.ParametersEditor(traitState) { updated ->
-                            paramError = updated.additionalInfo ?: ""
-                        }
-                    }
-                },
-                confirmButton = {
-                    Button(onClick = {
-                        viewModel.insertTrait(traitState)
-                        onSuccess(traitState)
-                    }, enabled = name.isNotBlank() && paramError.isBlank()) {
-                        Text("Save")
-                    }
-                },
-                dismissButton = {
-                    Row {
-                        TextButton(onClick = {
-                            currentStep = TraitCreatorStep.ChooseFormat
-                        }) {
-                            Text("Back")
-                        }
+                            Spacer(modifier = Modifier.height(8.dp))
 
-                        TextButton(onClick = onDismiss) {
-                            Text("Cancel")
+                            selectedFormat?.getTraitFormatDefinition()
+                                ?.ParametersEditor(traitState) { updated ->
+                                    paramError = updated.additionalInfo ?: ""
+                                }
+
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.End
+                            ) {
+                                Row {
+                                    TextButton(onClick = {
+                                        currentStep = TraitCreatorStep.ChooseFormat
+                                    }) {
+                                        Text("Back")
+                                    }
+
+                                    TextButton(onClick = onDismiss) {
+                                        Text("Cancel")
+                                    }
+
+                                    Button(onClick = {
+                                        viewModel.insertTrait(traitState)
+                                        onSuccess(traitState)
+                                    }, enabled = traitName.isNotBlank() && paramError.isBlank()) {
+                                        Text("Save")
+                                    }
+                                }
+                            }
                         }
                     }
                 }
-            )
+            }
         }
     }
 }
