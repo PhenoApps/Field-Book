a55495b4c642b910f0b97f8e18e89f3a17f6745f
chaneylc <chaneylc@gmail.com>
2026-02-11 11:54:07 -0600

    fixed barcode scanning options

--- app/src/main/java/com/fieldbook/tracker/preferences/enums/BarcodeScanningOptions.kt
+++ app/src/main/java/com/fieldbook/tracker/preferences/enums/BarcodeScanningOptions.kt
@@ -1,8 +1,8 @@
 package com.fieldbook.tracker.preferences.enums
 
 sealed class BarcodeScanningOptions(val value: String) {
-    object EnterValue: BarcodeScanningOptions("0")
-    object Move: BarcodeScanningOptions("1")
-    object Ask: BarcodeScanningOptions("2")
-    object EnterIfNotId: BarcodeScanningOptions("3")
+    object Move: BarcodeScanningOptions("0")
+    object EnterValue: BarcodeScanningOptions("1")
+    object EnterIfNotId: BarcodeScanningOptions("2")
+    object Ask: BarcodeScanningOptions("3")
 }
