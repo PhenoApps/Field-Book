d8060e44a675ea2c8e07071674e911e855588536
Nahuel Soldevilla <n.soldevilla@integratedbreeding.net>
2026-04-27 12:06:54 -0300

    IBP-7142 clear settings in common code

--- shared/src/commonMain/kotlin/com/fieldbook/shared/utilities/DatabaseResetUtil.kt
+++ shared/src/commonMain/kotlin/com/fieldbook/shared/utilities/DatabaseResetUtil.kt
@@ -1,4 +1,19 @@
 package com.fieldbook.shared.utilities
 
-expect fun resetLocalDatabaseAndPreferences(): Boolean
+import com.fieldbook.shared.database.utils.DATABASE_NAME
+import com.fieldbook.shared.database.utils.PlatformEnv
+import com.fieldbook.shared.sqldelight.closeDatabase
+import com.russhwolf.settings.Settings
+import okio.FileSystem
+import okio.Path.Companion.toPath
+import okio.SYSTEM
 
+fun resetLocalDatabaseAndPreferences(): Boolean {
+    closeDatabase()
+    Settings().clear()
+
+    val databasePath = PlatformEnv.databaseFilePath(DATABASE_NAME).toPath()
+    FileSystem.SYSTEM.delete(databasePath, mustExist = false)
+
+    return !FileSystem.SYSTEM.exists(databasePath)
+}
