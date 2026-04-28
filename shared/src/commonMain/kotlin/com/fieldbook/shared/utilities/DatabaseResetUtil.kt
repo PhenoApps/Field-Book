80d22365e67461197b370a09672f2eecbba3339a
cuenyad <diego.cuenya@leafnode.io>
2026-04-28 17:30:54 -0300

    preserve DEFAULT_STORAGE_LOCATION_DIRECTORY when clearing settings during database reset.
    Issue: IBP-7142
    Reviewed-by: none

--- shared/src/commonMain/kotlin/com/fieldbook/shared/utilities/DatabaseResetUtil.kt
+++ shared/src/commonMain/kotlin/com/fieldbook/shared/utilities/DatabaseResetUtil.kt
@@ -2,6 +2,7 @@ package com.fieldbook.shared.utilities
 
 import com.fieldbook.shared.database.utils.DATABASE_NAME
 import com.fieldbook.shared.database.utils.PlatformEnv
+import com.fieldbook.shared.preferences.GeneralKeys
 import com.fieldbook.shared.sqldelight.closeDatabase
 import com.russhwolf.settings.Settings
 import okio.FileSystem
@@ -10,7 +11,20 @@ import okio.SYSTEM
 
 fun resetLocalDatabaseAndPreferences(): Boolean {
     closeDatabase()
-    Settings().clear()
+
+    val settings = Settings()
+    val configuredStorageDirectory = settings.getString(
+        GeneralKeys.DEFAULT_STORAGE_LOCATION_DIRECTORY.key,
+        ""
+    )
+
+    settings.clear()
+    if (configuredStorageDirectory.isNotBlank()) {
+        settings.putString(
+            GeneralKeys.DEFAULT_STORAGE_LOCATION_DIRECTORY.key,
+            configuredStorageDirectory
+        )
+    }
 
     val databasePath = PlatformEnv.databaseFilePath(DATABASE_NAME).toPath()
     FileSystem.SYSTEM.delete(databasePath, mustExist = false)
