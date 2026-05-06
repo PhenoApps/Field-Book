fa5236533d722bcb69202e2d5c816fc1525c73d9
cuenyad <diego.cuenya@leafnode.io>
2026-05-06 15:32:49 -0300

    Import `MaterialTheme` in `TraitCreatorDialogFragment`.
    Issue: IBP-7142
    Reviewed-by: none

--- shared/src/commonMain/kotlin/com/fieldbook/shared/screens/trait/TraitCreatorDialogFragment.kt
+++ shared/src/commonMain/kotlin/com/fieldbook/shared/screens/trait/TraitCreatorDialogFragment.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
