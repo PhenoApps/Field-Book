0a42284688708e40b8465ffa1de7833b13c8661f
trife <twrife@clemson.edu>
2026-07-28 16:54:39 -0400

    Update PhotoTraitLayout.kt

--- app/src/main/java/com/fieldbook/tracker/traits/PhotoTraitLayout.kt
+++ app/src/main/java/com/fieldbook/tracker/traits/PhotoTraitLayout.kt
@@ -10,7 +10,6 @@ import android.os.Looper
 import android.provider.MediaStore
 import android.util.Log
 import android.util.AttributeSet
-import android.util.Log
 import android.util.Size
 import androidx.annotation.OptIn
 import androidx.camera.camera2.interop.ExperimentalCamera2Interop
